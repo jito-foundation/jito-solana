77cbc63f7b01a9c2f130a7ad68e63c9d622ce81c
Christian Kamm <mail@ckamm.de>
2022-05-06 21:16:13 +0200

    FindPacketSenderStake: Improve metrics (#24971)
    - separate names for vote and non-vote thread
    - time unit postfixes (one is in ns!)

--- core/src/tpu.rs
+++ core/src/tpu.rs
@@ -126,6 +126,7 @@ impl Tpu {
             find_packet_sender_stake_sender,
             bank_forks.clone(),
             cluster_info.clone(),
+            "tpu-find-packet-sender-stake",
         );
 
         let (vote_find_packet_sender_stake_sender, vote_find_packet_sender_stake_receiver) =
@@ -136,6 +137,7 @@ impl Tpu {
             vote_find_packet_sender_stake_sender,
             bank_forks.clone(),
             cluster_info.clone(),
+            "tpu-vote-find-packet-sender-stake",
         );
 
         let (verified_sender, verified_receiver) = unbounded();
