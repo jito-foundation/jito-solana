e8c1fa2b9f358108ec56cbfdb3dbb78e5fb8f4df
Zano <segfaultdoctor@protonmail.com>
2022-05-15 14:21:43 -0400

    mv some bundles related items into sdk

---
+++ sdk/src/bundle/utils.rs
@@ -0,0 +1,23 @@
+use {
+    crate::bundle::error::BundleExecutionError, solana_sdk::transaction::TransactionError,
+    std::result,
+};
+
+type LockResult = result::Result<(), TransactionError>;
+
+/// Checks that preparing a bundle gives an acceptable batch back
+pub fn check_bundle_lock_results(
+    lock_results: &[LockResult],
+) -> Option<(BundleExecutionError, usize)> {
+    for (i, res) in lock_results.iter().enumerate() {
+        match res {
+            Ok(())
+            | Err(TransactionError::AccountInUse)
+            | Err(TransactionError::BundleNotContinuous) => {}
+            Err(e) => {
+                return Some((e.clone().into(), i));
+            }
+        }
+    }
+    None
+}
