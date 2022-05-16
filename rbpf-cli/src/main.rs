solana-labs/solana

rbpf-cli: add coverage flag
- adds DWARF parser using gimli
- adds coverage module to derive an execution trace to line numbers
- adds a gcov module to export a coverage profile to gcov intermediate JSON format
- add basic CLI logger

rbpf-cli/src/main.rs
@@ -1,5 +1,12 @@
+mod coverage;
+mod gcov;
+mod logger;
+
 use {
+    crate::{coverage::Coverage, gcov::GcovIntermediate, logger::Logger},
     clap::{crate_version, App, Arg},
+    goblin::elf::Elf,
+    log::*,
     serde::{Deserialize, Serialize},
     serde_json::Result,
     solana_bpf_loader_program::{
@@ -44,15 +51,14 @@ struct Input {
 fn load_accounts(path: &Path) -> Result<Input> {
     let file = File::open(path).unwrap();
     let input: Input = serde_json::from_reader(file)?;
-    eprintln!("Program input:");
-    eprintln!("accounts {:?}", &input.accounts);
-    eprintln!("instruction_data {:?}", &input.instruction_data);
-    eprintln!("----------------------------------------");
+    info!("Program input:");
+    info!("accounts {:?}", &input.accounts);
+    info!("instruction_data {:?}", &input.instruction_data);
+    info!("----------------------------------------");
     Ok(input)
 }
 
 fn main() {
-    solana_logger::setup();
     let matches = App::new("Solana BPF CLI")
         .version(crate_version!())
         .author("Solana Maintainers <maintainers@solana.foundation>")
@@ -147,6 +153,11 @@ native machine code before execting it in the virtual machine.",
                 .long("trace"),
         )
         .arg(
+            Arg::new("verbose")
+                .about("Show additional information")
+                .long("verbose"),
+        )
+        .arg(
             Arg::new("profile")
                 .about("Output profile to 'profile.dot' file using tracing instrumentation")
                 .short('p')
@@ -167,10 +178,19 @@ native machine code before execting it in the virtual machine.",
                 .takes_value(true)
                 .possible_values(&["json", "json-compact"]),
         )
+        .arg(
+            Arg::new("coverage")
+                .about("Output coverage profile")
+                .short('c')
+                .long("coverage"),
+        )
         .get_matches();
 
+    log::set_boxed_logger(Box::new(Logger::new(matches.is_present("verbose")))).unwrap();
     let config = Config {
-        enable_instruction_tracing: matches.is_present("trace") || matches.is_present("profile"),
+        enable_instruction_tracing: matches.is_present("trace")
+            || matches.is_present("profile")
+            || matches.is_present("coverage"),
         enable_symbol_and_section_labels: true,
         ..Config::default()
     };
@@ -320,27 +340,51 @@ native machine code before execting it in the virtual machine.",
             println!("{}", serde_json::to_string(&output).unwrap());
         }
         _ => {
-            println!("Program output:");
-            println!("{:?}", output);
+            info!("Program output:");
+            info!("{:?}", output);
         }
     }
 
     if matches.is_present("trace") {
-        eprintln!("Trace is saved in trace.out");
+        info!("Trace is saved in trace.out");
         let mut file = File::create("trace.out").unwrap();
         vm.get_tracer()
             .write(&mut file, analysis.analyze())
             .unwrap();
     }
     if matches.is_present("profile") {
-        eprintln!("Profile is saved in profile.dot");
-        let tracer = &vm.get_tracer();
+        info!("Profile is saved in profile.dot");
+        let tracer = vm.get_tracer();
         let analysis = analysis.analyze();
         let dynamic_analysis = DynamicAnalysis::new(tracer, analysis);
         let mut file = File::create("profile.dot").unwrap();
         analysis
             .visualize_graphically(&mut file, Some(&dynamic_analysis))
             .unwrap();
+    }
+    if matches.is_present("coverage") {
+        // Read ELF to buffer.
+        let elf_size = file.seek(SeekFrom::End(0)).unwrap();
+        file.seek(SeekFrom::Start(0)).unwrap();
+        let mut elf_bytes = Vec::<u8>::with_capacity(elf_size as usize);
+        file.read_to_end(&mut elf_bytes)
+            .expect("failed to read ELF");
+        // Parse ELF.
+        let elf = Elf::parse(&elf_bytes).expect("invalid ELF");
+        // Get register state trace.
+        let tracer = vm.get_tracer();
+        // Create coverage profile.
+        match Coverage::from_trace(&elf_bytes, &elf, &tracer.log) {
+            Err(err) => error!("Failed to build coverage profile: {}", err),
+            Ok(cov) => {
+                info!("Code coverage is saved in coverage.json");
+                trace!("{:?}", cov);
+                let mut file = File::create("coverage.json").unwrap();
+                let gcov: GcovIntermediate = (&cov).into();
+                serde_json::to_writer_pretty(&mut file, &gcov)
+                    .expect("Failed to write coverage JSON");
+            }
+        }
     }
 }
 

jito-foundation/jito-solana

rbpf-cli/src/main.rs
@@ -1,5 +1,12 @@
+mod coverage;
+mod gcov;
+mod logger;
+
 use {
+    crate::{coverage::Coverage, gcov::GcovIntermediate, logger::Logger},
     clap::{crate_version, Arg, Command},
+    goblin::elf::Elf,
+    log::*,
     serde::{Deserialize, Serialize},
     serde_json::Result,
     solana_bpf_loader_program::{
@@ -44,10 +51,10 @@ struct Input {
 fn load_accounts(path: &Path) -> Result<Input> {
     let file = File::open(path).unwrap();
     let input: Input = serde_json::from_reader(file)?;
-    eprintln!("Program input:");
-    eprintln!("accounts {:?}", &input.accounts);
-    eprintln!("instruction_data {:?}", &input.instruction_data);
-    eprintln!("----------------------------------------");
+    info!("Program input:");
+    info!("accounts {:?}", &input.accounts);
+    info!("instruction_data {:?}", &input.instruction_data);
+    info!("----------------------------------------");
     Ok(input)
 }
 
@@ -147,6 +154,11 @@ native machine code before execting it in the virtual machine.",
                 .long("trace"),
         )
         .arg(
+            Arg::new("verbose")
+                .help("Show additional information")
+                .long("verbose"),
+        )
+        .arg(
             Arg::new("profile")
                 .help("Output profile to 'profile.dot' file using tracing instrumentation")
                 .short('p')
@@ -167,10 +179,19 @@ native machine code before execting it in the virtual machine.",
                 .takes_value(true)
                 .possible_values(&["json", "json-compact"]),
         )
+        .arg(
+            Arg::new("coverage")
+                .help("Output coverage profile")
+                .short('c')
+                .long("coverage"),
+        )
         .get_matches();
 
+    log::set_boxed_logger(Box::new(Logger::new(matches.is_present("verbose")))).unwrap();
     let config = Config {
-        enable_instruction_tracing: matches.is_present("trace") || matches.is_present("profile"),
+        enable_instruction_tracing: matches.is_present("trace")
+            || matches.is_present("profile")
+            || matches.is_present("coverage"),
         enable_symbol_and_section_labels: true,
         ..Config::default()
     };
@@ -322,27 +343,51 @@ native machine code before execting it in the virtual machine.",
             println!("{}", serde_json::to_string(&output).unwrap());
         }
         _ => {
-            println!("Program output:");
-            println!("{:?}", output);
+            info!("Program output:");
+            info!("{:?}", output);
         }
     }
 
     if matches.is_present("trace") {
-        eprintln!("Trace is saved in trace.out");
+        info!("Trace is saved in trace.out");
         let mut file = File::create("trace.out").unwrap();
         vm.get_tracer()
             .write(&mut file, analysis.analyze())
             .unwrap();
     }
     if matches.is_present("profile") {
-        eprintln!("Profile is saved in profile.dot");
-        let tracer = &vm.get_tracer();
+        info!("Profile is saved in profile.dot");
+        let tracer = vm.get_tracer();
         let analysis = analysis.analyze();
         let dynamic_analysis = DynamicAnalysis::new(tracer, analysis);
         let mut file = File::create("profile.dot").unwrap();
         analysis
             .visualize_graphically(&mut file, Some(&dynamic_analysis))
             .unwrap();
+    }
+    if matches.is_present("coverage") {
+        // Read ELF to buffer.
+        let elf_size = file.seek(SeekFrom::End(0)).unwrap();
+        file.seek(SeekFrom::Start(0)).unwrap();
+        let mut elf_bytes = Vec::<u8>::with_capacity(elf_size as usize);
+        file.read_to_end(&mut elf_bytes)
+            .expect("failed to read ELF");
+        // Parse ELF.
+        let elf = Elf::parse(&elf_bytes).expect("invalid ELF");
+        // Get register state trace.
+        let tracer = vm.get_tracer();
+        // Create coverage profile.
+        match Coverage::from_trace(&elf_bytes, &elf, &tracer.log) {
+            Err(err) => error!("Failed to build coverage profile: {}", err),
+            Ok(cov) => {
+                info!("Code coverage is saved in coverage.json");
+                trace!("{:?}", cov);
+                let mut file = File::create("coverage.json").unwrap();
+                let gcov: GcovIntermediate = (&cov).into();
+                serde_json::to_writer_pretty(&mut file, &gcov)
+                    .expect("Failed to write coverage JSON");
+            }
+        }
     }
 }
 