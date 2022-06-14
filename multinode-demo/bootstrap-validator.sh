f11515fa896bfc7155bdff7817e5abc640ecad0d
0xspl.iff <bill@jito.wtf>
2022-06-14 12:54:20 -0700

    local fixes

--- multinode-demo/bootstrap-validator.sh
+++ multinode-demo/bootstrap-validator.sh
@@ -103,7 +103,7 @@ while [[ -n $1 ]]; do
     elif [[ $1 == --skip-require-tower ]]; then
       maybeRequireTower=false
       shift
-    elif [[ $1 == --validator_interface_address ]]; then
+    elif [[ $1 == --validator-interface-address ]]; then
       args+=("$1" "$2")
       shift 2
     elif [[ $1 == --tip-program-pubkey ]]; then
