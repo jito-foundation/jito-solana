b52ac86cc27487f0e15a5e649ed53f4f3ff4e49f
Lucas B <lucas@jito.wtf>
2022-06-14 19:34:22 -0500

    wip

--- core/benches/banking_stage.rs
+++ core/benches/banking_stage.rs
@@ -13,6 +13,7 @@ use {
         banking_stage::{BankingStage, BankingStageStats},
         leader_slot_banking_stage_metrics::LeaderSlotMetricsTracker,
         qos_service::QosService,
+        tip_manager::TipManager,
         unprocessed_packet_batches::*,
     },
     solana_entry::entry::{next_hash, Entry},
@@ -38,7 +39,7 @@ use {
     },
     solana_streamer::socket::SocketAddrSpace,
     std::{
-        sync::{atomic::Ordering, Arc, RwLock},
+        sync::{atomic::Ordering, Arc, Mutex, RwLock},
         time::{Duration, Instant},
     },
     test::Bencher,
@@ -240,6 +241,7 @@ fn bench_banking(bencher: &mut Bencher, tx_type: TransactionType) {
             s,
             Arc::new(RwLock::new(CostModel::default())),
             Arc::new(ConnectionCache::default()),
+            Arc::new(Mutex::new(TipManager::new(Pubkey::new_unique()))),
         );
         poh_recorder.lock().unwrap().set_bank(&bank);
 
