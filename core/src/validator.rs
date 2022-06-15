c3a18c243ba6b19fdd4acd457fa9832d642a84e4
Lucas B <lucas@jito.wtf>
2022-06-15 14:01:51 -0500

    wip

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -989,11 +989,13 @@ impl Validator {
             config.shred_receiver_address,
         );
 
-        let tip_program_pubkey = if config.voting_disabled {
-            Default::default()
-        } else {
-            config.tip_program_pubkey.unwrap()
-        };
+        // let tip_program_pubkey = if config.voting_disabled {
+        //     Pubkey::new_unique()
+        // } else {
+        //     // TODO (LB): fix
+        //     config.tip_program_pubkey.unwrap_or(Pubkey::new_unique())
+        // };
+        let tip_program_pubkey = config.tip_program_pubkey.unwrap_or(Pubkey::new_unique());
 
         let tpu = Tpu::new(
             &cluster_info,
