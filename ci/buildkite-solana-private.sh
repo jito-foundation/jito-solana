9388645371d179fc97c314021951a034eaf84b75
Lucas B <lucas@jito.wtf>
2022-06-19 17:34:12 -0500

    re-enable tests

--- ci/buildkite-solana-private.sh
+++ ci/buildkite-solana-private.sh
@@ -203,16 +203,16 @@ EOF
              ^programs/ \
              ^sdk/ \
       ; then
-        annotate --style warning --context test-stable-perf \
-                      "test-stable-perf is currently disabled (LB)"
-#    cat >> "$output_file" <<"EOF"
-#  - command: "ci/test-stable-perf.sh"
-#    name: "stable-perf"
-#    timeout_in_minutes: 35
-#    artifact_paths: "log-*.txt"
-#    agents:
-#      - "queue=sol-private"
-#EOF
+#        annotate --style warning --context test-stable-perf \
+#                      "test-stable-perf is currently disabled (LB)"
+    cat >> "$output_file" <<"EOF"
+  - command: "ci/test-stable-perf.sh"
+    name: "stable-perf"
+    timeout_in_minutes: 35
+    artifact_paths: "log-*.txt"
+    agents:
+      - "queue=sol-private"
+EOF
   else
     annotate --style info --context test-stable-perf \
       "Stable-perf skipped as no relevant files were modified"
@@ -251,9 +251,9 @@ EOF
              ^ci/test-stable.sh \
              ^sdk/ \
       ; then
-        annotate --style warning --context test-wasm \
-                              "test-wasm is currently disabled (LB)"
-#    command_step wasm ". ci/rust-version.sh; ci/docker-run.sh \$\$rust_stable_docker_image ci/test-wasm.sh" 20
+#        annotate --style warning --context test-wasm \
+#                              "test-wasm is currently disabled (LB)"
+    command_step wasm ". ci/rust-version.sh; ci/docker-run.sh \$\$rust_stable_docker_image ci/test-wasm.sh" 20
   else
     annotate --style info --context test-wasm \
       "wasm skipped as no relevant files were modified"
