25a039c4330a572e9d4cbf1ff956a690274e1b84
Zano <segfaultdoctor@protonmail.com>
2022-06-21 19:03:17 -0400

    add tip-distributor binaries

---
+++ tip-distributor/src/bin/stake-meta-generator.rs
@@ -0,0 +1,64 @@
+//! This binary is responsible for generating a JSON file that contains meta-data about stake
+//! & delegations given a ledger snapshot directory. The JSON file is structured as an array
+//! of [StakeMeta] objects.
+
+use {
+    clap::Parser,
+    log::*,
+    solana_sdk::{clock::Slot, pubkey::Pubkey},
+    solana_tip_distributor::{self, stake_meta_generator_workflow::run_workflow},
+    std::{
+        fs::{self},
+        path::PathBuf,
+        process::exit,
+    },
+};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Ledger path.
+    #[clap(long, env, parse(try_from_str = Args::ledger_path_parser))]
+    ledger_path: PathBuf,
+
+    /// The tip-distribution program id.
+    #[clap(long, env)]
+    tip_distribution_program_id: Pubkey,
+
+    /// Path to JSON file populated with the [StakeMetaCollection] object.
+    #[clap(long, env)]
+    out_path: String,
+
+    /// The expected base58 encoded snapshot bank hash.
+    #[clap(long, env)]
+    snapshot_bank_hash: String,
+
+    /// The expected snapshot slot.
+    #[clap(long, env)]
+    snapshot_slot: Slot,
+}
+
+impl Args {
+    fn ledger_path_parser(ledger_path: &str) -> Result<PathBuf, &'static str> {
+        Ok(fs::canonicalize(&ledger_path).unwrap_or_else(|err| {
+            error!("Unable to access ledger path '{}': {}", ledger_path, err);
+            exit(1);
+        }))
+    }
+}
+
+fn main() {
+    env_logger::init();
+    info!("Starting stake-meta-generator...");
+
+    let args: Args = Args::parse();
+
+    run_workflow(
+        &args.ledger_path,
+        args.snapshot_bank_hash,
+        args.snapshot_slot,
+        args.tip_distribution_program_id,
+        args.out_path,
+    )
+    .expect("Workflow failed.");
+}
