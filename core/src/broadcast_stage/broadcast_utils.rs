ce99c243f6bff95d7f1564724a77404f0c7143bf
buffalu <85544055+buffalu@users.noreply.github.com>
2022-09-01 14:38:49 -0500

    Lb/v1.11 backport (#127)

--- core/src/broadcast_stage/broadcast_utils.rs
+++ core/src/broadcast_stage/broadcast_utils.rs
@@ -36,18 +36,27 @@ pub(super) fn recv_slot_entries(receiver: &Receiver<WorkingBankEntry>) -> Result
     let timer = Duration::new(1, 0);
     let recv_start = Instant::now();
 
-    let (mut bank, (entry, mut last_tick_height)) = receiver.recv_timeout(timer)?;
+    let WorkingBankEntry {
+        mut bank,
+        entries_ticks,
+    } = receiver.recv_timeout(timer)?;
 
-    let mut entries = vec![entry];
+    let ticks: Vec<u64> = entries_ticks.iter().map(|(_, tick)| *tick).collect();
+    let mut entries: Vec<Entry> = entries_ticks.into_iter().map(|(entry, _)| entry).collect();
 
+    let mut last_tick_height = *ticks.iter().max().unwrap();
     assert!(last_tick_height <= bank.max_tick_height());
 
-    // Drain channel
+    // Drain channel until max tick height or no more entries
     while last_tick_height != bank.max_tick_height() {
-        let (try_bank, (entry, tick_height)) = match receiver.try_recv() {
+        let WorkingBankEntry {
+            bank: try_bank,
+            entries_ticks,
+        } = match receiver.try_recv() {
             Ok(working_bank_entry) => working_bank_entry,
             Err(_) => break,
         };
+
         // If the bank changed, that implies the previous slot was interrupted and we do not have to
         // broadcast its entries.
         if try_bank.slot() != bank.slot() {
@@ -55,19 +64,25 @@ pub(super) fn recv_slot_entries(receiver: &Receiver<WorkingBankEntry>) -> Result
             entries.clear();
             bank = try_bank;
         }
-        last_tick_height = tick_height;
-        entries.push(entry);
+        last_tick_height = entries_ticks.iter().map(|(_, tick)| *tick).max().unwrap();
+        entries.extend(entries_ticks.into_iter().map(|(entry, _)| entry));
         assert!(last_tick_height <= bank.max_tick_height());
     }
 
-    let mut serialized_batch_byte_count = serialized_size(&entries)?;
+    let mut serialized_batch_byte_count = 0;
+    for entry in &entries {
+        serialized_batch_byte_count += serialized_size(entry)?;
+    }
 
     // Wait up to `ENTRY_COALESCE_DURATION` to try to coalesce entries into a 32 shred batch
     let mut coalesce_deadline = Instant::now() + ENTRY_COALESCE_DURATION;
     while last_tick_height != bank.max_tick_height()
         && serialized_batch_byte_count < target_serialized_batch_byte_count
     {
-        let (try_bank, (entry, tick_height)) = match receiver.recv_deadline(coalesce_deadline) {
+        let WorkingBankEntry {
+            bank: try_bank,
+            entries_ticks,
+        } = match receiver.recv_deadline(coalesce_deadline) {
             Ok(working_bank_entry) => working_bank_entry,
             Err(_) => break,
         };
@@ -80,17 +95,18 @@ pub(super) fn recv_slot_entries(receiver: &Receiver<WorkingBankEntry>) -> Result
             bank = try_bank;
             coalesce_deadline = Instant::now() + ENTRY_COALESCE_DURATION;
         }
-        last_tick_height = tick_height;
-        let entry_bytes = serialized_size(&entry)?;
-        serialized_batch_byte_count += entry_bytes;
-        entries.push(entry);
+        last_tick_height = entries_ticks.iter().map(|(_, tick)| *tick).max().unwrap();
+
+        for (entry, _) in &entries_ticks {
+            serialized_batch_byte_count += serialized_size(entry)?;
+        }
+        entries.extend(entries_ticks.into_iter().map(|(entry, _)| entry));
         assert!(last_tick_height <= bank.max_tick_height());
     }
 
-    let time_elapsed = recv_start.elapsed();
     Ok(ReceiveResults {
         entries,
-        time_elapsed,
+        time_elapsed: recv_start.elapsed(),
         bank,
         last_tick_height,
     })
@@ -138,7 +154,11 @@ mod tests {
             .map(|i| {
                 let entry = Entry::new(&last_hash, 1, vec![tx.clone()]);
                 last_hash = entry.hash;
-                s.send((bank1.clone(), (entry.clone(), i))).unwrap();
+                s.send(WorkingBankEntry {
+                    bank: bank1.clone(),
+                    entries_ticks: vec![(entry.clone(), i)],
+                })
+                .unwrap();
                 entry
             })
             .collect();
@@ -172,11 +192,18 @@ mod tests {
                 last_hash = entry.hash;
                 // Interrupt slot 1 right before the last tick
                 if tick_height == expected_last_height {
-                    s.send((bank2.clone(), (entry.clone(), tick_height)))
-                        .unwrap();
+                    s.send(WorkingBankEntry {
+                        bank: bank2.clone(),
+                        entries_ticks: vec![(entry.clone(), tick_height)],
+                    })
+                    .unwrap();
                     Some(entry)
                 } else {
-                    s.send((bank1.clone(), (entry, tick_height))).unwrap();
+                    s.send(WorkingBankEntry {
+                        bank: bank1.clone(),
+                        entries_ticks: vec![(entry, tick_height)],
+                    })
+                    .unwrap();
                     None
                 }
             })
--- core/src/tpu.rs
+++ core/src/tpu.rs
@@ -5,15 +5,23 @@ use {
     crate::{
         banking_stage::BankingStage,
         broadcast_stage::{BroadcastStage, BroadcastStageType, RetransmitSlotsReceiver},
+        bundle_account_locker::BundleAccountLocker,
+        bundle_stage::BundleStage,
         cluster_info_vote_listener::{
             ClusterInfoVoteListener, GossipDuplicateConfirmedSlotsSender,
             GossipVerifiedVoteHashSender, VerifiedVoteSender, VoteTracker,
         },
         fetch_stage::FetchStage,
         find_packet_sender_stake_stage::FindPacketSenderStakeStage,
+        proxy::{
+            block_engine_stage::{BlockEngineConfig, BlockEngineStage},
+            fetch_stage_manager::FetchStageManager,
+            relayer_stage::{RelayerConfig, RelayerStage},
+        },
         sigverify::TransactionSigVerifier,
         sigverify_stage::SigVerifyStage,
         staked_nodes_updater_service::StakedNodesUpdaterService,
+        tip_manager::{TipManager, TipManagerConfig},
     },
     crossbeam_channel::{unbounded, Receiver},
     solana_client::connection_cache::ConnectionCache,
@@ -35,7 +43,8 @@ use {
         streamer::StakedNodes,
     },
     std::{
-        net::UdpSocket,
+        collections::HashSet,
+        net::{SocketAddr, UdpSocket},
         sync::{atomic::AtomicBool, Arc, RwLock},
         thread,
     },
@@ -59,6 +68,9 @@ pub struct Tpu {
     fetch_stage: FetchStage,
     sigverify_stage: SigVerifyStage,
     vote_sigverify_stage: SigVerifyStage,
+    maybe_relayer_stage: Option<RelayerStage>,
+    maybe_block_engine_stage: Option<BlockEngineStage>,
+    maybe_fetch_stage_manager: Option<FetchStageManager>,
     banking_stage: BankingStage,
     cluster_info_vote_listener: ClusterInfoVoteListener,
     broadcast_stage: BroadcastStage,
@@ -67,6 +79,7 @@ pub struct Tpu {
     find_packet_sender_stake_stage: FindPacketSenderStakeStage,
     vote_find_packet_sender_stake_stage: FindPacketSenderStakeStage,
     staked_nodes_updater_service: StakedNodesUpdaterService,
+    bundle_stage: BundleStage,
 }
 
 impl Tpu {
@@ -97,6 +110,10 @@ impl Tpu {
         keypair: &Keypair,
         log_messages_bytes_limit: Option<usize>,
         staked_nodes: &Arc<RwLock<StakedNodes>>,
+        maybe_block_engine_config: Option<BlockEngineConfig>,
+        maybe_relayer_config: Option<RelayerConfig>,
+        tip_manager_config: TipManagerConfig,
+        shred_receiver_address: Option<SocketAddr>,
     ) -> Self {
         let TpuSockets {
             transactions: transactions_sockets,
@@ -107,7 +124,17 @@ impl Tpu {
             transactions_forwards_quic: transactions_forwards_quic_sockets,
         } = sockets;
 
+        let (packet_intercept_sender, packet_intercept_receiver) = unbounded();
         let (packet_sender, packet_receiver) = unbounded();
+
+        // If there's a relayer, we need to redirect packets to the interceptor
+        // If not, they can flow straight through
+        let packet_send_channel = if maybe_relayer_config.is_some() {
+            packet_intercept_sender
+        } else {
+            packet_sender.clone()
+        };
+
         let (vote_packet_sender, vote_packet_receiver) = unbounded();
         let (forwarded_packet_sender, forwarded_packet_receiver) = unbounded();
         let fetch_stage = FetchStage::new_with_sender(
@@ -115,7 +142,7 @@ impl Tpu {
             tpu_forwards_sockets,
             tpu_vote_sockets,
             exit,
-            &packet_sender,
+            &packet_send_channel,
             &vote_packet_sender,
             &forwarded_packet_sender,
             forwarded_packet_receiver,
@@ -157,7 +184,7 @@ impl Tpu {
             transactions_quic_sockets,
             keypair,
             cluster_info.my_contact_info().tpu.ip(),
-            packet_sender,
+            packet_send_channel,
             exit.clone(),
             MAX_QUIC_CONNECTIONS_PER_PEER,
             staked_nodes.clone(),
@@ -182,7 +209,7 @@ impl Tpu {
         .unwrap();
 
         let sigverify_stage = {
-            let verifier = TransactionSigVerifier::new(verified_sender);
+            let verifier = TransactionSigVerifier::new(verified_sender.clone());
             SigVerifyStage::new(find_packet_sender_stake_receiver, verifier, "tpu-verifier")
         };
 
@@ -197,6 +224,39 @@ impl Tpu {
                 "tpu-vote-verifier",
             )
         };
+
+        let (bundle_sender, bundle_receiver) = unbounded();
+        let maybe_block_engine_stage = maybe_block_engine_config.map(|block_engine_config| {
+            BlockEngineStage::new(
+                block_engine_config,
+                bundle_sender,
+                cluster_info.clone(),
+                packet_sender.clone(),
+                verified_sender.clone(),
+                exit.clone(),
+            )
+        });
+
+        let (heartbeat_tx, heartbeat_rx) = unbounded();
+        let maybe_fetch_stage_manager = maybe_relayer_config.as_ref().map(|_| {
+            FetchStageManager::new(
+                cluster_info.clone(),
+                heartbeat_rx,
+                packet_intercept_receiver,
+                packet_sender.clone(),
+                exit.clone(),
+            )
+        });
+        let maybe_relayer_stage = maybe_relayer_config.map(|relayer_config| {
+            RelayerStage::new(
+                relayer_config,
+                cluster_info.clone(),
+                heartbeat_tx,
+                packet_sender,
+                verified_sender,
+                exit.clone(),
+            )
+        });
 
         let (verified_gossip_vote_packets_sender, verified_gossip_vote_packets_receiver) =
             unbounded();
@@ -216,18 +276,42 @@ impl Tpu {
             cluster_confirmed_slot_sender,
         );
 
+        let tip_manager = TipManager::new(tip_manager_config);
+
+        let bundle_account_locker = BundleAccountLocker::default();
+
+        // tip accounts can't be used in BankingStage to avoid someone from stealing tips mid-slot.
+        // it also helps reduce surface area for potential account contention
+        let mut blacklisted_accounts = HashSet::new();
+        blacklisted_accounts.insert(tip_manager.tip_payment_config_pubkey());
+        blacklisted_accounts.insert(tip_manager.tip_payment_program_id());
+        blacklisted_accounts.extend(tip_manager.get_tip_accounts());
         let banking_stage = BankingStage::new(
             cluster_info,
             poh_recorder,
             verified_receiver,
             verified_tpu_vote_packets_receiver,
             verified_gossip_vote_packets_receiver,
-            transaction_status_sender,
-            replay_vote_sender,
+            transaction_status_sender.clone(),
+            replay_vote_sender.clone(),
             cost_model.clone(),
             log_messages_bytes_limit,
             connection_cache.clone(),
             bank_forks.clone(),
+            blacklisted_accounts,
+            bundle_account_locker.clone(),
+        );
+
+        let bundle_stage = BundleStage::new(
+            cluster_info,
+            poh_recorder,
+            transaction_status_sender,
+            replay_vote_sender,
+            cost_model.clone(),
+            bundle_receiver,
+            exit.clone(),
+            tip_manager,
+            bundle_account_locker,
         );
 
         let broadcast_stage = broadcast_type.new_broadcast_stage(
@@ -239,12 +323,16 @@ impl Tpu {
             blockstore.clone(),
             bank_forks,
             shred_version,
+            shred_receiver_address,
         );
 
         Self {
             fetch_stage,
             sigverify_stage,
             vote_sigverify_stage,
+            maybe_block_engine_stage,
+            maybe_relayer_stage,
+            maybe_fetch_stage_manager,
             banking_stage,
             cluster_info_vote_listener,
             broadcast_stage,
@@ -253,6 +341,7 @@ impl Tpu {
             find_packet_sender_stake_stage,
             vote_find_packet_sender_stake_stage,
             staked_nodes_updater_service,
+            bundle_stage,
         }
     }
 
@@ -268,7 +357,19 @@ impl Tpu {
             self.staked_nodes_updater_service.join(),
             self.tpu_quic_t.join(),
             self.tpu_forwards_quic_t.join(),
+            self.bundle_stage.join(),
         ];
+
+        if let Some(relayer_stage) = self.maybe_relayer_stage {
+            relayer_stage.join()?;
+        }
+        if let Some(block_engine_stage) = self.maybe_block_engine_stage {
+            block_engine_stage.join()?;
+        }
+        if let Some(fetch_stage_manager) = self.maybe_fetch_stage_manager {
+            fetch_stage_manager.join()?;
+        }
+
         let broadcast_result = self.broadcast_stage.join();
         for result in results {
             result?;
