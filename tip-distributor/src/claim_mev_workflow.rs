a5831f47dced859d5ed2b199caacff402ae2b51d
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2022-11-23 21:51:43 -0800

    fmt

--- tip-distributor/src/claim_mev_workflow.rs
+++ tip-distributor/src/claim_mev_workflow.rs
@@ -1,10 +1,12 @@
-use solana_program::stake::state::StakeState;
 use {
     crate::{read_json_from_file, send_transactions_with_retry, GeneratedMerkleTreeCollection},
     anchor_lang::{AccountDeserialize, InstructionData, ToAccountMetas},
     log::{debug, info, warn},
     solana_client::{nonblocking::rpc_client::RpcClient, rpc_request::RpcError},
-    solana_program::{fee_calculator::DEFAULT_TARGET_LAMPORTS_PER_SIGNATURE, system_program},
+    solana_program::{
+        fee_calculator::DEFAULT_TARGET_LAMPORTS_PER_SIGNATURE, stake::state::StakeState,
+        system_program,
+    },
     solana_rpc_client_api::client_error,
     solana_sdk::{
         commitment_config::CommitmentConfig,
