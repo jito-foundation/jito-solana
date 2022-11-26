d201f1aa2b62bdc4a074288816441de327c74ce0
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2022-11-26 08:10:59 -0800

    update msg

--- tip-distributor/src/claim_mev_workflow.rs
+++ tip-distributor/src/claim_mev_workflow.rs
@@ -68,8 +68,8 @@ pub fn claim_mev_tips(
             let desired_balance = node_count as u64 * (min_rent_per_claim + DEFAULT_TARGET_LAMPORTS_PER_SIGNATURE);
             if start_balance < desired_balance {
                 let sol_deposit = (desired_balance - start_balance + LAMPORTS_PER_SOL - 1) / LAMPORTS_PER_SOL; //rounds up to nearest sol
-                panic!("Expected to have at least {} lamports, current balance is {} lamports, deposit {} SOL to {} to continue.",
-                       desired_balance, start_balance, sol_deposit, &keypair.pubkey())
+                panic!("Expected to have at least {} lamports in {}, current balance is {} lamports, deposit {} SOL to continue.",
+                       desired_balance, &keypair.pubkey(), start_balance, sol_deposit)
             }
         }
         let stake_acct_min_rent = rpc_client.get_minimum_balance_for_rent_exemption(StakeState::size_of()).await.expect("Failed to calculate min rent");
