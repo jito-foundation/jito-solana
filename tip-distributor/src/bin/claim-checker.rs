a13d5d1760ab4e96e3b3eee9fec93e49eb47379a
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2023-02-08 14:00:49 -0800

    calculate diff

--- tip-distributor/src/bin/claim-checker.rs
+++ tip-distributor/src/bin/claim-checker.rs
@@ -40,25 +40,30 @@ fn main() {
         .collect::<Vec<_>>();
 
     for tda in tda_metas {
-        if let Some(tree) = tda_to_merkle_tree.get(&tda.tip_distribution_pubkey) {
-            let actual_claim: u64 = tree.tree_nodes.iter().map(|t| t.amount).sum();
+        match tda_to_merkle_tree.get(&tda.tip_distribution_pubkey) {
+            Some(tree) => {
+                let calculated_claim: u64 = tree.tree_nodes.iter().map(|t| t.amount).sum();
 
-            if actual_claim != tree.max_total_claim {
-                error!("Calculation error: calculated total claim={actual_claim}, expected total claim={}, tda={}", tree.max_total_claim, tda.tip_distribution_pubkey);
-                continue;
+                if calculated_claim != tree.max_total_claim {
+                    let diff = tree.max_total_claim - calculated_claim;
+                    error!("Calculation error: calculated total claim={calculated_claim}, expected total claim={}, diff={diff}, tda={}", tree.max_total_claim, tda.tip_distribution_pubkey);
+                    continue;
+                }
+
+                if calculated_claim != tda.total_tips {
+                    let diff = tda.total_tips - calculated_claim;
+                    error!("Calculation error: calculated total claim={calculated_claim}, calculated total tips={}, diff={diff}, tda={}", tda.total_tips, tda.tip_distribution_pubkey);
+                    continue;
+                }
+
+                info!("{} tda is gucci", tda.tip_distribution_pubkey);
             }
-
-            if tda.total_tips != actual_claim {
-                error!("Calculation error: calculated total tips={}, calculated total claim = {actual_claim}, tda={}", tda.total_tips, tda.tip_distribution_pubkey);
-                continue;
+            None => {
+                warn!(
+                    "{} TDA not found in merkle trees",
+                    tda.tip_distribution_pubkey
+                );
             }
-
-            info!("{} tda is gucci", tda.tip_distribution_pubkey);
-        } else {
-            warn!(
-                "{} TDA not found in merkle trees",
-                tda.tip_distribution_pubkey
-            );
         }
     }
 }
