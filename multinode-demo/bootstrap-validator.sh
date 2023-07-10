614359b53ae4846074313d67e827a7782929c4c6
Edgar <edgar@jito.wtf>
2023-07-10 11:38:11 -0400

    add geyser-plugin-config path

--- multinode-demo/bootstrap-validator.sh
+++ multinode-demo/bootstrap-validator.sh
@@ -145,6 +145,9 @@ while [[ -n $1 ]]; do
     elif [[ $1 = --log-messages-bytes-limit ]]; then
       args+=("$1" "$2")
       shift 2
+    elif [[ $1 = --geyser-plugin-config ]]; then
+      arg+=("$1" "$2")
+      shift 2
     else
       echo "Unknown argument: $1"
       $program --help
