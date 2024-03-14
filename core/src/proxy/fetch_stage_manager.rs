528e3b175eebc412b464ef0a91e009acd7c2caa2
buffalu <85544055+buffalu@users.noreply.github.com>
2024-03-14 13:03:21 -0700

    Fix: Ensure set contact info to UDP port instead of QUIC (Backport #601 to v1.18) (#602)

--- core/src/proxy/fetch_stage_manager.rs
+++ core/src/proxy/fetch_stage_manager.rs
@@ -108,10 +108,10 @@ impl FetchStageManager {
                             fetch_connected = true;
                             pending_disconnect = false;
 
-                            // unwrap safe here bc contact_info.tpu(Protocol::QUIC) and contact_info.tpu_forwards(Protocol::QUIC)
-                            // are checked on startup
-                            if let Err(e) = Self::set_tpu_addresses(&cluster_info, my_fallback_contact_info.tpu(Protocol::QUIC).unwrap(), my_fallback_contact_info.tpu_forwards(Protocol::QUIC).unwrap()) {
-                                error!("error setting tpu or tpu_fwd to ({:?}, {:?}), error: {:?}", my_fallback_contact_info.tpu(Protocol::QUIC).unwrap(), my_fallback_contact_info.tpu_forwards(Protocol::QUIC).unwrap(), e);
+                            // yes, using UDP here is extremely confusing for the validator
+                            // since the entire network is running QUIC. However, it's correct.
+                            if let Err(e) = Self::set_tpu_addresses(&cluster_info, my_fallback_contact_info.tpu(Protocol::UDP).unwrap(), my_fallback_contact_info.tpu_forwards(Protocol::UDP).unwrap()) {
+                                error!("error setting tpu or tpu_fwd to ({:?}, {:?}), error: {:?}", my_fallback_contact_info.tpu(Protocol::UDP).unwrap(), my_fallback_contact_info.tpu_forwards(Protocol::UDP).unwrap(), e);
                             }
                             heartbeats_received = 0;
                         }
