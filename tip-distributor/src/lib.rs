5ba7091f7d63b5cb20d31d4d1194f2fccaf208e5
Jed <4679729+jedleggett@users.noreply.github.com>
2024-03-14 23:17:53 -0700

    1k tips

--- tip-distributor/src/lib.rs
+++ tip-distributor/src/lib.rs
@@ -603,7 +603,7 @@ pub async fn send_until_blockhash_expires(
                             .as_bytes(),
                         &[],
                     ),
-                    transfer(&keypair.pubkey(), &tip_account, 100000),
+                    transfer(&keypair.pubkey(), &tip_account, 1000),
                 ],
                 Some(&keypair.pubkey()),
                 &[keypair],
