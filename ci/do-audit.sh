4cc7b90484ab8054090b78ea59645b5904e57efb
Yihau Chen <yihau.chen@icloud.com>
2024-10-02 23:56:43 +0800

    v2.0: ignore tonic audit report (#3051)
    * fix audit
    
    * add more comment

--- ci/do-audit.sh
+++ ci/do-audit.sh
@@ -33,6 +33,12 @@ cargo_audit_ignores=(
 
   # curve25519-dalek
   --ignore RUSTSEC-2024-0344
+
+  # tonic
+  # When using tonic::transport::Server there is a remote DoS attack that can cause
+  # the server to exit cleanly on accepting a tcp/tls stream.
+  # Ignoring because we do not use this functionality.
+  --ignore RUSTSEC-2024-0376
 )
 scripts/cargo-for-all-lock-files.sh audit "${cargo_audit_ignores[@]}" | $dep_tree_filter
 # we want the `cargo audit` exit code, not `$dep_tree_filter`'s
