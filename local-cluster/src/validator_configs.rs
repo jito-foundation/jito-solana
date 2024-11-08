5b10a3142c6f330e806edb37ea30b4a2e5ab1676
buffalu <85544055+buffalu@users.noreply.github.com>
2024-11-07 21:00:11 -0600

    Separate out broadcast + retransmit shredstream (#703)

--- local-cluster/src/validator_configs.rs
+++ local-cluster/src/validator_configs.rs
@@ -76,6 +76,7 @@ pub fn safe_clone_config(config: &ValidatorConfig) -> ValidatorConfig {
         relayer_config: config.relayer_config.clone(),
         block_engine_config: config.block_engine_config.clone(),
         shred_receiver_address: config.shred_receiver_address.clone(),
+        shred_retransmit_receiver_address: config.shred_retransmit_receiver_address.clone(),
         tip_manager_config: config.tip_manager_config.clone(),
         preallocated_bundle_cost: config.preallocated_bundle_cost,
     }
