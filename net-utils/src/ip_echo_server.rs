343474b77ea79e499cc1cd1eec768642fca0e131
Kevin Heavey <heaveykevin2@gmail.com>
2024-12-03 16:38:26 +0400

    remove solana-sdk from solana-net-utils (#3843)

--- net-utils/src/ip_echo_server.rs
+++ net-utils/src/ip_echo_server.rs
@@ -2,7 +2,7 @@ use {
     crate::{bind_to_unspecified, HEADER_LENGTH, IP_ECHO_SERVER_RESPONSE_LENGTH},
     log::*,
     serde_derive::{Deserialize, Serialize},
-    solana_sdk::deserialize_utils::default_on_eof,
+    solana_serde::default_on_eof,
     std::{
         io,
         net::{IpAddr, SocketAddr},
