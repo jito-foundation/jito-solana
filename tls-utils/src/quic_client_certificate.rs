6cb7ca873848516deb4f5b563f54b1a25e1d7dd8
Jon C <me@jonc.dev>
2024-12-04 14:34:00 +0100

    tls-utils: Make publishable, remove sdk dep (#3909)
    #### Problem
    
    The new tls-utils crate is a great extraction of re-used logic, but it
    wasn't marked publishable, even though it will need to be published due
    to transitive dependencies from client crates.
    
    Also, it has an unnecessary dependency on solana-sdk.
    
    #### Summary of changes
    
    Remove the solana-sdk dependency and use solana-keypair directly. And
    make the crate publishable.

--- tls-utils/src/quic_client_certificate.rs
+++ tls-utils/src/quic_client_certificate.rs
@@ -1,7 +1,7 @@
 use {
     crate::new_dummy_x509_certificate,
     rustls::pki_types::{CertificateDer, PrivateKeyDer},
-    solana_sdk::signature::Keypair,
+    solana_keypair::Keypair,
 };
 
 pub struct QuicClientCertificate {
