b2a7a647e14cb60ac3ba1f1d95026d6ab7066d8b
Kevin Heavey <heaveykevin2@gmail.com>
2024-12-05 18:46:40 +0400

    remove solana-program from transaction-context dev deps (#3944)

--- sdk/transaction-context/src/lib.rs
+++ sdk/transaction-context/src/lib.rs
@@ -22,23 +22,23 @@ use {
     },
 };
 
-// Inlined to avoid solana_program dep
+// Inlined to avoid solana_system_interface dep
 #[cfg(not(target_os = "solana"))]
 const MAX_PERMITTED_DATA_LENGTH: u64 = 10 * 1024 * 1024;
 #[cfg(test)]
 static_assertions::const_assert_eq!(
     MAX_PERMITTED_DATA_LENGTH,
-    solana_program::system_instruction::MAX_PERMITTED_DATA_LENGTH
+    solana_system_interface::MAX_PERMITTED_DATA_LENGTH
 );
 
-// Inlined to avoid solana_program dep
+// Inlined to avoid solana_system_interface dep
 #[cfg(not(target_os = "solana"))]
 const MAX_PERMITTED_ACCOUNTS_DATA_ALLOCATIONS_PER_TRANSACTION: i64 =
     MAX_PERMITTED_DATA_LENGTH as i64 * 2;
 #[cfg(test)]
 static_assertions::const_assert_eq!(
     MAX_PERMITTED_ACCOUNTS_DATA_ALLOCATIONS_PER_TRANSACTION,
-    solana_program::system_instruction::MAX_PERMITTED_ACCOUNTS_DATA_ALLOCATIONS_PER_TRANSACTION
+    solana_system_interface::MAX_PERMITTED_ACCOUNTS_DATA_ALLOCATIONS_PER_TRANSACTION
 );
 
 // Inlined to avoid solana_account_info dep
