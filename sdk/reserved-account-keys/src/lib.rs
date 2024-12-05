87fa1f87835d645cf36d2869e88a13f53fe1d3a5
Kevin Heavey <heaveykevin2@gmail.com>
2024-12-05 15:24:16 +0400

    remove solana-program from solana-reserved-account-keys dev deps (#3907)

--- sdk/reserved-account-keys/src/lib.rs
+++ sdk/reserved-account-keys/src/lib.rs
@@ -185,10 +185,7 @@ lazy_static! {
 #[cfg(test)]
 mod tests {
     #![allow(deprecated)]
-    use {
-        super::*,
-        solana_program::{message::legacy::BUILTIN_PROGRAMS_KEYS, sysvar::ALL_IDS},
-    };
+    use {super::*, solana_message::legacy::BUILTIN_PROGRAMS_KEYS, solana_sysvar::ALL_IDS};
 
     #[test]
     fn test_is_reserved() {
