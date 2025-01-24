a1d26ad4580881b9127368684c35fd0d5f4dad36
Emanuele Cesena <emanuele.cesena@gmail.com>
2025-01-24 13:55:22 -0800

    quic: increase timeout and keep alive (#4585)
    * quic: increase timeout and keep alive
    
    * quic: add comment to QUIC_MAX_TIMEOUT
    
    ---------
    
    Co-authored-by: Lijun Wang <83639177+lijunwangs@users.noreply.github.com>

--- sdk/quic-definitions/src/lib.rs
+++ sdk/quic-definitions/src/lib.rs
@@ -16,8 +16,12 @@ pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
 // forwarded packets from staked nodes.
 pub const QUIC_MAX_STAKED_CONCURRENT_STREAMS: usize = 512;
 
-pub const QUIC_MAX_TIMEOUT: Duration = Duration::from_secs(2);
-pub const QUIC_KEEP_ALIVE: Duration = Duration::from_secs(1);
+// Connection idle timeout, and keep alive.
+// Quic will close the connection after QUIC_MAX_TIMEOUT,
+// and send a ping every QUIC_KEEP_ALIVE.
+// These shouldn't be too low to avoid unnecessary ping traffic.
+pub const QUIC_MAX_TIMEOUT: Duration = Duration::from_secs(60);
+pub const QUIC_KEEP_ALIVE: Duration = Duration::from_secs(45);
 
 // Disable Quic send fairness.
 // When set to false, streams are still scheduled based on priority,
