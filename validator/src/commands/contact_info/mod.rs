ddec7bdbcf308a853d464f865ae4962acbc2b9cd
Yihau Chen <yihau.chen@icloud.com>
2025-02-14 17:31:19 +0800

    agave-validator: add args tests for contact-info (#4893)
    * agave-validator: add args tests for contact-info
    
    * ContactInfoArg -> ContactInfoArgs
    
    * arg -> expected_arg
    
    * extract 'contact-info' to COMMAND
    
    * test with meaningful words
    
    * add default test

--- validator/src/commands/contact_info/mod.rs
+++ validator/src/commands/contact_info/mod.rs
@@ -1,11 +1,26 @@
 use {
-    crate::{admin_rpc_service, cli::DefaultArgs},
+    crate::{admin_rpc_service, cli::DefaultArgs, commands::FromClapArgMatches},
     clap::{App, Arg, ArgMatches, SubCommand},
     std::{path::Path, process::exit},
 };
 
+const COMMAND: &str = "contact-info";
+
+#[derive(Debug, PartialEq)]
+pub struct ContactInfoArgs {
+    pub output: Option<String>,
+}
+
+impl FromClapArgMatches for ContactInfoArgs {
+    fn from_clap_arg_match(matches: &ArgMatches) -> Self {
+        ContactInfoArgs {
+            output: matches.value_of("output").map(String::from),
+        }
+    }
+}
+
 pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
-    SubCommand::with_name("contact-info")
+    SubCommand::with_name(COMMAND)
         .about("Display the validator's contact info")
         .arg(
             Arg::with_name("output")
@@ -18,7 +33,8 @@ pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &Path) {
-    let output_mode = matches.value_of("output");
+    let contact_info_args = ContactInfoArgs::from_clap_arg_match(matches);
+
     let admin_client = admin_rpc_service::connect(ledger_path);
     let contact_info = admin_rpc_service::runtime()
         .block_on(async move { admin_client.await?.contact_info().await })
@@ -26,13 +42,62 @@ pub fn execute(matches: &ArgMatches, ledger_path: &Path) {
             eprintln!("Contact info query failed: {err}");
             exit(1);
         });
-    if let Some(mode) = output_mode {
-        match mode {
+    if let Some(mode) = contact_info_args.output {
+        match mode.as_str() {
             "json" => println!("{}", serde_json::to_string_pretty(&contact_info).unwrap()),
             "json-compact" => print!("{}", serde_json::to_string(&contact_info).unwrap()),
             _ => unreachable!(),
         }
     } else {
         print!("{contact_info}");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::commands::tests::{
+            verify_args_struct_by_command, verify_args_struct_by_command_is_error,
+        },
+    };
+
+    #[test]
+    fn verify_args_struct_by_command_contact_info_output_json() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![COMMAND, "--output", "json"],
+            ContactInfoArgs {
+                output: Some("json".to_string()),
+            },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_contact_info_output_json_compact() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![COMMAND, "--output", "json-compact"],
+            ContactInfoArgs {
+                output: Some("json-compact".to_string()),
+            },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_contact_info_output_default() {
+        verify_args_struct_by_command(
+            command(&DefaultArgs::default()),
+            vec![COMMAND],
+            ContactInfoArgs { output: None },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_contact_info_output_invalid() {
+        verify_args_struct_by_command_is_error::<ContactInfoArgs>(
+            command(&DefaultArgs::default()),
+            vec![COMMAND, "--output", "invalid_output_type"],
+        );
     }
 }
--- validator/src/commands/mod.rs
+++ validator/src/commands/mod.rs
@@ -11,3 +11,29 @@ pub mod set_log_filter;
 pub mod set_public_address;
 pub mod staked_nodes_overrides;
 pub mod wait_for_restart_window;
+
+pub trait FromClapArgMatches {
+    fn from_clap_arg_match(matches: &clap::ArgMatches) -> Self;
+}
+
+#[cfg(test)]
+pub mod tests {
+    use std::fmt::Debug;
+
+    pub fn verify_args_struct_by_command<T>(app: clap::App, vec: Vec<&str>, expected_arg: T)
+    where
+        T: crate::commands::FromClapArgMatches + PartialEq + Debug,
+    {
+        let matches = app.get_matches_from(vec);
+        let result = T::from_clap_arg_match(&matches);
+        assert_eq!(result, expected_arg);
+    }
+
+    pub fn verify_args_struct_by_command_is_error<T>(app: clap::App, vec: Vec<&str>)
+    where
+        T: crate::commands::FromClapArgMatches + PartialEq + Debug,
+    {
+        let matches = app.get_matches_from_safe(vec);
+        assert!(matches.is_err());
+    }
+}
