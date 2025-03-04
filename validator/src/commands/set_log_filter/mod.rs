131bc18e05f81cdce21308bd82df4dc61c0f63e9
Yihau Chen <yihau.chen@icloud.com>
2025-03-04 12:27:31 +0800

    agave-validator: add args tests for set-log-filter (#4923)
    * remove unused defualt config
    
    * add COMMAND const
    
    * add SetLogFilterArgs

--- validator/src/commands/set_log_filter/mod.rs
+++ validator/src/commands/set_log_filter/mod.rs
@@ -1,11 +1,26 @@
 use {
-    crate::{admin_rpc_service, cli::DefaultArgs},
-    clap::{value_t_or_exit, App, Arg, ArgMatches, SubCommand},
+    crate::{admin_rpc_service, commands::FromClapArgMatches},
+    clap::{value_t, App, Arg, ArgMatches, SubCommand},
     std::path::Path,
 };
 
-pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
-    SubCommand::with_name("set-log-filter")
+const COMMAND: &str = "set-log-filter";
+
+#[derive(Debug, PartialEq)]
+pub struct SetLogFilterArgs {
+    pub filter: String,
+}
+
+impl FromClapArgMatches for SetLogFilterArgs {
+    fn from_clap_arg_match(matches: &ArgMatches) -> Result<Self, String> {
+        Ok(SetLogFilterArgs {
+            filter: value_t!(matches, "filter", String).map_err(|e| e.to_string())?,
+        })
+    }
+}
+
+pub fn command<'a>() -> App<'a, 'a> {
+    SubCommand::with_name(COMMAND)
         .about("Adjust the validator log filter")
         .arg(
             Arg::with_name("filter")
@@ -17,9 +32,37 @@ pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &Path) -> Result<(), String> {
-    let filter = value_t_or_exit!(matches, "filter", String);
+    let set_log_filter_args = SetLogFilterArgs::from_clap_arg_match(matches)?;
+
     let admin_client = admin_rpc_service::connect(ledger_path);
     admin_rpc_service::runtime()
-        .block_on(async move { admin_client.await?.set_log_filter(filter).await })
+        .block_on(async move {
+            admin_client
+                .await?
+                .set_log_filter(set_log_filter_args.filter)
+                .await
+        })
         .map_err(|err| format!("set log filter request failed: {err}"))
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, crate::commands::tests::verify_args_struct_by_command};
+
+    #[test]
+    fn verify_args_struct_by_command_set_log_filter_default() {
+        let matches = command().get_matches_from(vec![COMMAND]);
+        assert!(SetLogFilterArgs::from_clap_arg_match(&matches).is_err());
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_set_log_filter_with_filter() {
+        verify_args_struct_by_command(
+            command(),
+            vec![COMMAND, "expected_filter_value"],
+            SetLogFilterArgs {
+                filter: "expected_filter_value".to_string(),
+            },
+        );
+    }
 }
