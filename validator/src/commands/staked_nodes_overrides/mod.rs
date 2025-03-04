b31e5f47f9b9190a3c566f6d13c3f37422961071
Yihau Chen <yihau.chen@icloud.com>
2025-03-04 20:09:18 +0800

    agave-validator: add args tests for staked-nodes-overrides (#4903)
    * agave-validator: add args tests for staked-nodes-overrides
    
    * StakedNodesOverridesArg -> StakedNodesOverridesArgs
    
    * remove unused default_args
    
    * use path as a PathBuf
    
    * Revert "use path as a PathBuf"
    
    This reverts commit 40a387545e8990c8ed4e02c275cd2af7c5b76783.

--- validator/src/commands/staked_nodes_overrides/mod.rs
+++ validator/src/commands/staked_nodes_overrides/mod.rs
@@ -1,11 +1,29 @@
 use {
-    crate::{admin_rpc_service, cli::DefaultArgs},
+    crate::{admin_rpc_service, commands::FromClapArgMatches},
     clap::{App, Arg, ArgMatches, SubCommand},
     std::path::Path,
 };
 
-pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
-    SubCommand::with_name("staked-nodes-overrides")
+const COMMAND: &str = "staked-nodes-overrides";
+
+#[derive(Debug, PartialEq)]
+pub struct StakedNodesOverridesArgs {
+    pub path: String,
+}
+
+impl FromClapArgMatches for StakedNodesOverridesArgs {
+    fn from_clap_arg_match(matches: &ArgMatches) -> Result<Self, String> {
+        Ok(StakedNodesOverridesArgs {
+            path: matches
+                .value_of("path")
+                .expect("path is required")
+                .to_string(),
+        })
+    }
+}
+
+pub fn command<'a>() -> App<'a, 'a> {
+    SubCommand::with_name(COMMAND)
         .about("Overrides stakes of specific node identities.")
         .arg(
             Arg::with_name("path")
@@ -22,15 +40,44 @@ pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &Path) -> Result<(), String> {
-    let path = matches.value_of("path").expect("path is required");
+    let staked_nodes_overrides_args = StakedNodesOverridesArgs::from_clap_arg_match(matches)?;
 
     let admin_client = admin_rpc_service::connect(ledger_path);
     admin_rpc_service::runtime()
         .block_on(async move {
             admin_client
                 .await?
-                .set_staked_nodes_overrides(path.to_string())
+                .set_staked_nodes_overrides(staked_nodes_overrides_args.path)
                 .await
         })
         .map_err(|err| format!("set staked nodes override request failed: {err}"))
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::commands::tests::{
+            verify_args_struct_by_command, verify_args_struct_by_command_is_error,
+        },
+    };
+
+    #[test]
+    fn verify_args_struct_by_command_staked_nodes_overrides_default() {
+        verify_args_struct_by_command_is_error::<StakedNodesOverridesArgs>(
+            command(),
+            vec![COMMAND],
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_staked_nodes_overrides_path() {
+        verify_args_struct_by_command(
+            command(),
+            vec![COMMAND, "test.json"],
+            StakedNodesOverridesArgs {
+                path: "test.json".to_string(),
+            },
+        );
+    }
 }
