17da52f77ab7479882ae77793067afdddacecb14
Yihau Chen <yihau.chen@icloud.com>
2025-03-10 21:15:17 +0800

    agave-validator: add args tests for repair-shred-from-peer (#4990)
    * extract COMMAND
    
    * extract RepairShredFromPeerArgs
    
    * remove unused default args

--- validator/src/commands/repair_shred_from_peer/mod.rs
+++ validator/src/commands/repair_shred_from_peer/mod.rs
@@ -1,13 +1,32 @@
 use {
-    crate::{admin_rpc_service, cli::DefaultArgs},
-    clap::{value_t, value_t_or_exit, App, Arg, ArgMatches, SubCommand},
+    crate::{admin_rpc_service, commands::FromClapArgMatches},
+    clap::{value_t, App, Arg, ArgMatches, SubCommand},
     solana_clap_utils::input_validators::{is_parsable, is_pubkey},
     solana_sdk::pubkey::Pubkey,
     std::path::Path,
 };
 
-pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
-    SubCommand::with_name("repair-shred-from-peer")
+const COMMAND: &str = "repair-shred-from-peer";
+
+#[derive(Debug, PartialEq)]
+pub struct RepairShredFromPeerArgs {
+    pub pubkey: Option<Pubkey>,
+    pub slot: u64,
+    pub shred: u64,
+}
+
+impl FromClapArgMatches for RepairShredFromPeerArgs {
+    fn from_clap_arg_match(matches: &ArgMatches) -> Result<Self, String> {
+        Ok(RepairShredFromPeerArgs {
+            pubkey: value_t!(matches, "pubkey", Pubkey).ok(),
+            slot: value_t!(matches, "slot", u64).map_err(|_| "slot is not a valid number")?,
+            shred: value_t!(matches, "shred", u64).map_err(|_| "shred is not a valid number")?,
+        })
+    }
+}
+
+pub fn command<'a>() -> App<'a, 'a> {
+    SubCommand::with_name(COMMAND)
         .about("Request a repair from the specified validator")
         .arg(
             Arg::with_name("pubkey")
@@ -37,16 +56,76 @@ pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
 }
 
 pub fn execute(matches: &ArgMatches, ledger_path: &Path) -> Result<(), String> {
-    let pubkey = value_t!(matches, "pubkey", Pubkey).ok();
-    let slot = value_t_or_exit!(matches, "slot", u64);
-    let shred_index = value_t_or_exit!(matches, "shred", u64);
+    let RepairShredFromPeerArgs {
+        pubkey,
+        slot,
+        shred,
+    } = RepairShredFromPeerArgs::from_clap_arg_match(matches)?;
+
     let admin_client = admin_rpc_service::connect(ledger_path);
     admin_rpc_service::runtime()
         .block_on(async move {
             admin_client
                 .await?
-                .repair_shred_from_peer(pubkey, slot, shred_index)
+                .repair_shred_from_peer(pubkey, slot, shred)
                 .await
         })
         .map_err(|err| format!("repair shred from peer request failed: {err}"))
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, crate::commands::tests::verify_args_struct_by_command, std::str::FromStr};
+
+    #[test]
+    fn verify_args_struct_by_command_repair_shred_from_peer_missing_slot() {
+        let app = command();
+        let matches = app.get_matches_from(vec![COMMAND]);
+        let args = RepairShredFromPeerArgs::from_clap_arg_match(&matches);
+        assert_eq!(args, Err("slot is not a valid number".to_string()));
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_repair_shred_from_peer_missing_shred() {
+        let app = command();
+        let matches = app.get_matches_from(vec![COMMAND, "--slot", "1"]);
+        let args = RepairShredFromPeerArgs::from_clap_arg_match(&matches);
+        assert_eq!(args, Err("shred is not a valid number".to_string()));
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_repair_shred_from_peer_missing_pubkey() {
+        verify_args_struct_by_command(
+            command(),
+            vec![COMMAND, "--slot", "1", "--shred", "2"],
+            RepairShredFromPeerArgs {
+                pubkey: None,
+                slot: 1,
+                shred: 2,
+            },
+        );
+    }
+
+    #[test]
+    fn verify_args_struct_by_command_repair_shred_from_peer_with_pubkey() {
+        verify_args_struct_by_command(
+            command(),
+            vec![
+                COMMAND,
+                "--slot",
+                "1",
+                "--shred",
+                "2",
+                "--pubkey",
+                "ch1do11111111111111111111111111111111111111",
+            ],
+            RepairShredFromPeerArgs {
+                pubkey: Some(
+                    Pubkey::from_str("ch1do11111111111111111111111111111111111111").unwrap(),
+                ),
+                slot: 1,
+                shred: 2,
+            },
+        );
+    }
 }
