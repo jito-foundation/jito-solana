2eb058dd5d6c2a7643abf9380c2b3d2e5633993f
Yihau Chen <yihau.chen@icloud.com>
2025-03-12 22:06:39 +0800

    ci: use exec in ci/docker-run-default-image.sh (#5228)
    ci: use exec in ci/docker-run-default-image.sh to handle SIGTERM correctly

--- ci/docker-run-default-image.sh
+++ ci/docker-run-default-image.sh
@@ -7,4 +7,4 @@ here="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck disable=SC1091
 source "$here/docker/env.sh"
 
-"$here/docker-run.sh" "${CI_DOCKER_IMAGE:?}" "$@"
+exec "$here/docker-run.sh" "${CI_DOCKER_IMAGE:?}" "$@"
