ee748358d83ddd7bc027887ad4f087aaa8de2511
Kevin Heavey <heaveykevin2@gmail.com>
2025-03-17 16:17:50 +0400

    extract rpc-client-types without reqwest dep (#4660)
    * extract rpc-client-api-core without reqwest dep
    
    * better description
    
    Co-authored-by: Jon C <me@jonc.dev>
    
    * rename dir to rpc-client-types
    
    * rename crate to solana-rpc-client-types
    
    * fix crate description
    
    Co-authored-by: Jon C <me@jonc.dev>
    
    ---------
    
    Co-authored-by: Jon C <me@jonc.dev>

--- rpc-client-api/src/lib.rs
+++ rpc-client-api/src/lib.rs
@@ -1,12 +1,9 @@
 #![allow(clippy::arithmetic_side_effects)]
 
 pub mod client_error;
-pub mod config;
 pub mod custom_error;
-pub mod error_object;
-pub mod filter;
-pub mod request;
 pub mod response;
+pub use solana_rpc_client_types::{config, error_object, filter, request};
 
 #[macro_use]
 extern crate serde_derive;
---
+++ rpc-client-types/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(clippy::arithmetic_side_effects)]
+
+pub mod config;
+pub mod error_object;
+pub mod filter;
+pub mod request;
+pub mod response;
+
+#[macro_use]
+extern crate serde_derive;
