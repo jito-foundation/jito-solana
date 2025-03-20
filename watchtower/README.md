a2d7eadfaea4528e2433dcd0b880018bbcb7fbfb
Nick Karter <95931668+nk4rter@users.noreply.github.com>
2025-03-20 20:04:17 +0200

    Support multiple RPC URLs in watchtower (#4748)
    * Accepting multiple urls in watchtower
    
    * Getting data from multiple endpoints
    
    * Collect and report multiple failures, add watchtower-reliability alert
    
    * Take exactly 1 or 3 URLs in watchtower
    
    * Make failure mesasge order stable, concatenate msgs for thresh skip
    
    * Send concatenated msg to the notifier
    
    * Cleaup: use original tuple instead of EndpointFailure
    
    * Ignoring failures if num_healthy >= min_agreeing_endpoints
    
    * Bring watchtower readme up to date
    
    * Unrealiability takes priority over other failures, trigger unrealiability if results are inconsistent
    
    * Add some minor fixes
    
    * Implement endpoint validation
    
    * Fix clippy issues

--- watchtower/README.md
+++ watchtower/README.md
@@ -1,17 +1,18 @@
-The `agave-watchtower` program is used to monitor the health of a cluster.  It
+The `agave-watchtower` program is used to monitor the health of a cluster. It
 periodically polls the cluster over an RPC API to confirm that the transaction
 count is advancing, new blockhashes are available, and no validators are
-delinquent.  Results are reported as InfluxDB metrics, with an optional push
+delinquent. Results are reported as InfluxDB metrics, with an optional push
 notification on sanity failure.
 
-If you only care about the health of one specific validator, the
+If you only care about the health of several specific validators, the
 `--validator-identity` command-line argument can be used to restrict failure
-notifications to issues only affecting that validator.
+notifications to issues only affecting that set of validators.
 
-If you do not want duplicate notifications, for example if you have elected to
-receive notifications by SMS the
-`--no-duplicate-notifications` command-line argument will suppress identical
-failure notifications.
+User can provide either 1 or 3 RPC URLs for the cluster via the `--url` or `--urls`
+command-line arguments respectively. 2 URLs are not accepted because it's not enough
+to have redundnacy, and more than 3 URLs are not accepted because there's little
+benefit from having more than 3. If 3 URLs are provided, at least 2 of them have to
+confirm health of a cluster.
 
 ### Metrics
 #### `watchtower-sanity`
--- watchtower/src/main.rs
+++ watchtower/src/main.rs
@@ -2,7 +2,7 @@
 #![allow(clippy::arithmetic_side_effects)]
 
 use {
-    clap::{crate_description, crate_name, value_t, value_t_or_exit, App, Arg},
+    clap::{crate_description, crate_name, value_t, value_t_or_exit, values_t, App, Arg},
     log::*,
     solana_clap_utils::{
         hidden_unless_forced,
@@ -29,7 +29,7 @@ struct Config {
     address_labels: HashMap<String, String>,
     ignore_http_bad_gateway: bool,
     interval: Duration,
-    json_rpc_url: String,
+    json_rpc_urls: Vec<String>,
     rpc_timeout: Duration,
     minimum_validator_identity_balance: u64,
     monitor_active_stake: bool,
@@ -37,6 +37,7 @@ struct Config {
     unhealthy_threshold: usize,
     validator_identity_pubkeys: Vec<Pubkey>,
     name_suffix: String,
+    acceptable_slot_range: u64,
 }
 
 fn get_config() -> Config {
@@ -84,7 +85,18 @@ fn get_config() -> Config {
                 .value_name("URL")
                 .takes_value(true)
                 .validator(is_url)
-                .help("JSON RPC URL for the cluster"),
+                .help("JSON RPC URL for the cluster (conflicts with --urls)"),
+        )
+        .arg(
+            Arg::with_name("json_rpc_urls")
+                .long("urls")
+                .value_name("URL")
+                .takes_value(true)
+                .validator(is_url)
+                .multiple(true)
+                .number_of_values(3)
+                .conflicts_with("json_rpc_url")
+                .help("JSON RPC URLs for the cluster (takes exactly 3 values, conflicts with --url)"),
         )
         .arg(
             Arg::with_name("rpc_timeout")
@@ -166,6 +178,15 @@ fn get_config() -> Config {
                 .default_value("")
                 .help("Add this string into all notification messages after \"agave-watchtower\"")
         )
+        .arg(
+            Arg::with_name("acceptable_slot_range")
+                .long("acceptable-slot-range")
+                .value_name("RANGE")
+                .takes_value(true)
+                .default_value("50")
+                .validator(is_parsable::<u64>)
+                .help("Acceptable range of slots for endpoints, checked at watchtower startup")
+        )
         .get_matches();
 
     let config = if let Some(config_file) = matches.value_of("config_file") {
@@ -181,8 +202,9 @@ fn get_config() -> Config {
         "minimum_validator_identity_balance",
         f64
     ));
-    let json_rpc_url =
-        value_t!(matches, "json_rpc_url", String).unwrap_or_else(|_| config.json_rpc_url.clone());
+    let json_rpc_urls = values_t!(matches, "json_rpc_urls", String).unwrap_or_else(|_| {
+        vec![value_t!(matches, "json_rpc_url", String).unwrap_or_else(|_| config.json_rpc_url)]
+    });
     let rpc_timeout = value_t_or_exit!(matches, "rpc_timeout", u64);
     let rpc_timeout = Duration::from_secs(rpc_timeout);
     let validator_identity_pubkeys: Vec<_> = pubkeys_of(&matches, "validator_identities")
@@ -197,11 +219,13 @@ fn get_config() -> Config {
 
     let name_suffix = value_t_or_exit!(matches, "name_suffix", String);
 
+    let acceptable_slot_range = value_t_or_exit!(matches, "acceptable_slot_range", u64);
+
     let config = Config {
         address_labels: config.address_labels,
         ignore_http_bad_gateway,
         interval,
-        json_rpc_url,
+        json_rpc_urls,
         rpc_timeout,
         minimum_validator_identity_balance,
         monitor_active_stake,
@@ -209,9 +233,10 @@ fn get_config() -> Config {
         unhealthy_threshold,
         validator_identity_pubkeys,
         name_suffix,
+        acceptable_slot_range,
     };
 
-    info!("RPC URL: {}", config.json_rpc_url);
+    info!("RPC URLs: {:?}", config.json_rpc_urls);
     info!(
         "Monitored validators: {:?}",
         config.validator_identity_pubkeys
@@ -243,141 +268,255 @@ fn get_cluster_info(
     ))
 }
 
+struct EndpointData {
+    rpc_client: RpcClient,
+    last_transaction_count: u64,
+    last_recent_blockhash: Hash,
+}
+
+fn query_endpoint(
+    config: &Config,
+    endpoint: &mut EndpointData,
+) -> client_error::Result<Option<(&'static str, String)>> {
+    info!("Querying {}", endpoint.rpc_client.url());
+
+    match get_cluster_info(config, &endpoint.rpc_client) {
+        Ok((transaction_count, recent_blockhash, vote_accounts, validator_balances)) => {
+            info!("Current transaction count: {}", transaction_count);
+            info!("Recent blockhash: {}", recent_blockhash);
+            info!("Current validator count: {}", vote_accounts.current.len());
+            info!(
+                "Delinquent validator count: {}",
+                vote_accounts.delinquent.len()
+            );
+
+            let mut failures = vec![];
+
+            let total_current_stake = vote_accounts
+                .current
+                .iter()
+                .map(|vote_account| vote_account.activated_stake)
+                .sum();
+            let total_delinquent_stake = vote_accounts
+                .delinquent
+                .iter()
+                .map(|vote_account| vote_account.activated_stake)
+                .sum();
+
+            let total_stake = total_current_stake + total_delinquent_stake;
+            let current_stake_percent = total_current_stake as f64 * 100. / total_stake as f64;
+            info!(
+                "Current stake: {:.2}% | Total stake: {}, current stake: {}, delinquent: {}",
+                current_stake_percent,
+                Sol(total_stake),
+                Sol(total_current_stake),
+                Sol(total_delinquent_stake)
+            );
+
+            if transaction_count > endpoint.last_transaction_count {
+                endpoint.last_transaction_count = transaction_count;
+            } else {
+                failures.push((
+                    "transaction-count",
+                    format!(
+                        "Transaction count is not advancing: {transaction_count} <= {0}",
+                        endpoint.last_transaction_count
+                    ),
+                ));
+            }
+
+            if recent_blockhash != endpoint.last_recent_blockhash {
+                endpoint.last_recent_blockhash = recent_blockhash;
+            } else {
+                failures.push((
+                    "recent-blockhash",
+                    format!("Unable to get new blockhash: {recent_blockhash}"),
+                ));
+            }
+
+            if config.monitor_active_stake
+                && current_stake_percent < config.active_stake_alert_threshold as f64
+            {
+                failures.push((
+                    "current-stake",
+                    format!("Current stake is {current_stake_percent:.2}%"),
+                ));
+            }
+
+            let mut validator_errors = vec![];
+            for validator_identity in config.validator_identity_pubkeys.iter() {
+                let formatted_validator_identity =
+                    format_labeled_address(&validator_identity.to_string(), &config.address_labels);
+                if vote_accounts
+                    .delinquent
+                    .iter()
+                    .any(|vai| vai.node_pubkey == *validator_identity.to_string())
+                {
+                    validator_errors.push(format!("{formatted_validator_identity} delinquent"));
+                } else if !vote_accounts
+                    .current
+                    .iter()
+                    .any(|vai| vai.node_pubkey == *validator_identity.to_string())
+                {
+                    validator_errors.push(format!("{formatted_validator_identity} missing"));
+                }
+
+                if let Some(balance) = validator_balances.get(validator_identity) {
+                    if *balance < config.minimum_validator_identity_balance {
+                        failures.push((
+                            "balance",
+                            format!("{} has {}", formatted_validator_identity, Sol(*balance)),
+                        ));
+                    }
+                }
+            }
+
+            if !validator_errors.is_empty() {
+                failures.push(("delinquent", validator_errors.join(",")));
+            }
+
+            for failure in &failures {
+                error!("{} sanity failure: {}", failure.0, failure.1);
+            }
+
+            Ok(failures.into_iter().next()) // Only report the first failure if any
+        }
+        Err(err) => {
+            if let client_error::ErrorKind::Reqwest(reqwest_err) = err.kind() {
+                if let Some(client_error::reqwest::StatusCode::BAD_GATEWAY) = reqwest_err.status() {
+                    if config.ignore_http_bad_gateway {
+                        warn!("Error suppressed: {}", err);
+                        return Ok(None);
+                    }
+                }
+            }
+            warn!("rpc-error: {}", err);
+            Err(err)
+        }
+    }
+}
+
+fn validate_endpoints(
+    config: &Config,
+    endpoints: &Vec<EndpointData>,
+) -> Result<(), Box<dyn error::Error>> {
+    info!("Validating endpoints...");
+
+    let mut max_slot = 0;
+    let mut min_slot = u64::MAX;
+
+    let mut opt_common_genesis_hash: Option<Hash> = None;
+
+    for endpoint in endpoints {
+        info!("Querying {}", endpoint.rpc_client.url());
+
+        let slot = endpoint.rpc_client.get_slot()?;
+        let genesis_hash = endpoint.rpc_client.get_genesis_hash()?;
+
+        info!("Genesis hash: {}", genesis_hash);
+        info!("Current slot: {}", slot);
+
+        max_slot = max_slot.max(slot);
+        min_slot = min_slot.min(slot);
+
+        if let Some(common_genesis_hash) = opt_common_genesis_hash {
+            if common_genesis_hash != genesis_hash {
+                return Err(
+                    "Endpoints don't aggree on genesis hash, have you mixed up clusters?".into(),
+                );
+            }
+        } else {
+            opt_common_genesis_hash = Some(genesis_hash);
+        }
+    }
+
+    if max_slot - min_slot > config.acceptable_slot_range {
+        return Err(format!(
+            "Endpoints slots are too far apart: Acceptable slot range: {}",
+            config.acceptable_slot_range,
+        )
+        .into());
+    }
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
     solana_logger::setup_with_default_filter();
     solana_metrics::set_panic_hook("watchtower", /*version:*/ None);
 
     let config = get_config();
 
-    let rpc_client = RpcClient::new_with_timeout(config.json_rpc_url.clone(), config.rpc_timeout);
+    let mut endpoints: Vec<_> = config
+        .json_rpc_urls
+        .iter()
+        .map(|url| EndpointData {
+            rpc_client: RpcClient::new_with_timeout(url, config.rpc_timeout),
+            last_transaction_count: 0,
+            last_recent_blockhash: Hash::default(),
+        })
+        .collect();
+
+    if let Err(err) = validate_endpoints(&config, &endpoints) {
+        error!("Endpoint validation failed: {}", err);
+        std::process::exit(1);
+    }
+
+    let min_agreeing_endpoints = endpoints.len() / 2 + 1;
+
     let notifier = Notifier::default();
-    let mut last_transaction_count = 0;
-    let mut last_recent_blockhash = Hash::default();
+
     let mut last_notification_msg = "".into();
     let mut num_consecutive_failures = 0;
     let mut last_success = Instant::now();
     let mut incident = Hash::new_unique();
 
     loop {
-        let failure = match get_cluster_info(&config, &rpc_client) {
-            Ok((transaction_count, recent_blockhash, vote_accounts, validator_balances)) => {
-                info!("Current transaction count: {}", transaction_count);
-                info!("Recent blockhash: {}", recent_blockhash);
-                info!("Current validator count: {}", vote_accounts.current.len());
-                info!(
-                    "Delinquent validator count: {}",
-                    vote_accounts.delinquent.len()
-                );
+        let mut failures = HashMap::new(); // test_name -> message
 
-                let mut failures = vec![];
+        let mut num_healthy = 0;
+        let mut num_reachable = 0;
 
-                let total_current_stake = vote_accounts
-                    .current
-                    .iter()
-                    .map(|vote_account| vote_account.activated_stake)
-                    .sum();
-                let total_delinquent_stake = vote_accounts
-                    .delinquent
-                    .iter()
-                    .map(|vote_account| vote_account.activated_stake)
-                    .sum();
-
-                let total_stake = total_current_stake + total_delinquent_stake;
-                let current_stake_percent = total_current_stake as f64 * 100. / total_stake as f64;
-                info!(
-                    "Current stake: {:.2}% | Total stake: {}, current stake: {}, delinquent: {}",
-                    current_stake_percent,
-                    Sol(total_stake),
-                    Sol(total_current_stake),
-                    Sol(total_delinquent_stake)
-                );
-
-                if transaction_count > last_transaction_count {
-                    last_transaction_count = transaction_count;
-                } else {
-                    failures.push((
-                        "transaction-count",
-                        format!(
-                            "Transaction count is not advancing: {transaction_count} <= {last_transaction_count}"
-                        ),
-                    ));
+        for endpoint in &mut endpoints {
+            match query_endpoint(&config, endpoint) {
+                Ok(None) => {
+                    num_healthy += 1;
+                    num_reachable += 1;
                 }
+                Ok(Some((failure_test_name, failure_error_message))) => {
+                    num_reachable += 1;
 
-                if recent_blockhash != last_recent_blockhash {
-                    last_recent_blockhash = recent_blockhash;
-                } else {
-                    failures.push((
-                        "recent-blockhash",
-                        format!("Unable to get new blockhash: {recent_blockhash}"),
-                    ));
+                    // Collecting only one failure of each type
+                    failures
+                        .entry(failure_test_name)
+                        .or_insert(failure_error_message.clone());
                 }
-
-                if config.monitor_active_stake
-                    && current_stake_percent < config.active_stake_alert_threshold as f64
-                {
-                    failures.push((
-                        "current-stake",
-                        format!("Current stake is {current_stake_percent:.2}%"),
-                    ));
-                }
-
-                let mut validator_errors = vec![];
-                for validator_identity in config.validator_identity_pubkeys.iter() {
-                    let formatted_validator_identity = format_labeled_address(
-                        &validator_identity.to_string(),
-                        &config.address_labels,
-                    );
-                    if vote_accounts
-                        .delinquent
-                        .iter()
-                        .any(|vai| vai.node_pubkey == *validator_identity.to_string())
-                    {
-                        validator_errors.push(format!("{formatted_validator_identity} delinquent"));
-                    } else if !vote_accounts
-                        .current
-                        .iter()
-                        .any(|vai| vai.node_pubkey == *validator_identity.to_string())
-                    {
-                        validator_errors.push(format!("{formatted_validator_identity} missing"));
-                    }
-
-                    if let Some(balance) = validator_balances.get(validator_identity) {
-                        if *balance < config.minimum_validator_identity_balance {
-                            failures.push((
-                                "balance",
-                                format!("{} has {}", formatted_validator_identity, Sol(*balance)),
-                            ));
-                        }
-                    }
-                }
-
-                if !validator_errors.is_empty() {
-                    failures.push(("delinquent", validator_errors.join(",")));
-                }
-
-                for failure in failures.iter() {
-                    error!("{} sanity failure: {}", failure.0, failure.1);
-                }
-                failures.into_iter().next() // Only report the first failure if any
+                Err(_) => {}
             }
-            Err(err) => {
-                let mut failure = Some(("rpc-error", err.to_string()));
+        }
 
-                if let client_error::ErrorKind::Reqwest(reqwest_err) = err.kind() {
-                    if let Some(client_error::reqwest::StatusCode::BAD_GATEWAY) =
-                        reqwest_err.status()
-                    {
-                        if config.ignore_http_bad_gateway {
-                            warn!("Error suppressed: {}", err);
-                            failure = None;
-                        }
-                    }
-                }
-                failure
+        if num_reachable < min_agreeing_endpoints {
+            failures.clear(); // Ignoring other failures when watchtower is unreliable
+
+            let watchtower_unreliable_msg = format!(
+                "Watchtower is unreliable, {} of {} RPC endpoints are reachable",
+                num_reachable,
+                endpoints.len()
+            );
+            failures.insert("watchtower-reliability", watchtower_unreliable_msg);
+        }
+
+        if num_healthy < min_agreeing_endpoints {
+            if failures.len() > 1 {
+                failures.clear(); // Ignoring other failures when watchtower is unreliable
+
+                let watchtower_unreliable_msg =
+                    "Watchtower is unreliable, RPC endpoints provide inconsistent information"
+                        .into();
+                failures.insert("watchtower-reliability", watchtower_unreliable_msg);
             }
-        };
 
-        if let Some((failure_test_name, failure_error_message)) = &failure {
+            let (failure_test_name, failure_error_message) = failures.iter().next().unwrap();
             let notification_msg = format!(
                 "agave-watchtower{}: Error: {}: {}",
                 config.name_suffix, failure_test_name, failure_error_message
