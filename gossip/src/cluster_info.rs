773b964f9299561dd9ed1613a059e143f118352c
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-05-01 21:00:54 +0300

    less parallelism in gossip verify (#6037)
    * less parallelism in gossip verify
    * rename also the function appropriately

--- gossip/src/cluster_info.rs
+++ gossip/src/cluster_info.rs
@@ -2129,7 +2129,7 @@ impl ClusterInfo {
                     return None;
                 }
             }
-            protocol.par_verify().then(|| {
+            protocol.verify().then(|| {
                 stats.packets_received_verified_count.add_relaxed(1);
                 (packet.meta().socket_addr(), protocol)
             })
--- gossip/src/protocol.rs
+++ gossip/src/protocol.rs
@@ -7,7 +7,6 @@ use {
         ping_pong::{self, Pong},
     },
     bincode::serialize,
-    rayon::prelude::*,
     serde::Serialize,
     solana_keypair::signable::Signable,
     solana_perf::packet::PACKET_DATA_SIZE,
@@ -91,11 +90,11 @@ impl Protocol {
 
     // Returns true if all signatures verify.
     #[must_use]
-    pub(crate) fn par_verify(&self) -> bool {
+    pub(crate) fn verify(&self) -> bool {
         match self {
             Self::PullRequest(_, caller) => caller.verify(),
-            Self::PullResponse(_, data) => data.par_iter().all(CrdsValue::verify),
-            Self::PushMessage(_, data) => data.par_iter().all(CrdsValue::verify),
+            Self::PullResponse(_, data) => data.iter().all(CrdsValue::verify),
+            Self::PushMessage(_, data) => data.iter().all(CrdsValue::verify),
             Self::PruneMessage(_, data) => data.verify(),
             Self::PingMessage(ping) => ping.verify(),
             Self::PongMessage(pong) => pong.verify(),
