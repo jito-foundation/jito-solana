d33fcb73c96d83b223d3f7c206f45dad1a512375
Maximilian Hubert <64627729+gap-editor@users.noreply.github.com>
2025-07-07 21:02:31 +0200

    fix:  typo in "occurred" (#6863)
    fix typo in "occurred"

--- runtime/src/bank_hash_cache.rs
+++ runtime/src/bank_hash_cache.rs
@@ -52,7 +52,7 @@ impl BankHashCache {
     /// Should only be used after `slots_dumped` is acquired from `dumped_slot_subscription` to
     /// guarantee synchronicity with `self.bank_forks`. Multiple calls to `hash` will only be
     /// consistent with each other if `slots_dumped` was not released in between, as otherwise a dump
-    /// could have occured inbetween.
+    /// could have occurred inbetween.
     pub fn hash(&mut self, slot: Slot, slots_dumped: &mut MutexGuard<bool>) -> Option<Hash> {
         if **slots_dumped {
             // We could be smarter and keep a fork cache to only clear affected slots from the cache,
