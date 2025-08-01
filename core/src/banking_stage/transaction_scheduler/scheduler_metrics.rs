76d4d3a573097c71fbdb6e6aaf93bda552eeed37
Andrew Fitzgerald <apfitzge@gmail.com>
2025-08-01 15:00:34 -0500

    SchedulingDetails::default min_starting_{buffer|queue}_size: usize::MAX (#7273)

--- core/src/banking_stage/transaction_scheduler/scheduler_metrics.rs
+++ core/src/banking_stage/transaction_scheduler/scheduler_metrics.rs
@@ -459,10 +459,10 @@ impl Default for SchedulingDetails {
         Self {
             last_report: Instant::now(),
             num_schedule_calls: 0,
-            min_starting_queue_size: 0,
+            min_starting_queue_size: usize::MAX,
             max_starting_queue_size: 0,
             sum_starting_queue_size: 0,
-            min_starting_buffer_size: 0,
+            min_starting_buffer_size: usize::MAX,
             max_starting_buffer_size: 0,
             sum_starting_buffer_size: 0,
             sum_num_scheduled: 0,
