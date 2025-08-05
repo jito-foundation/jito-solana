365844f0ecd53c472580aa2186f13a46786d6b56
Justin Starry <justin@anza.xyz>
2025-08-06 07:49:18 +0800

    initialize cluster slots with the current root bank (#7314)

--- core/src/cluster_slots_service.rs
+++ core/src/cluster_slots_service.rs
@@ -83,6 +83,11 @@ impl ClusterSlotsService {
         let mut cluster_slots_service_timing = ClusterSlotsServiceTiming::default();
         let mut last_stats = Instant::now();
         let mut epoch_specs = EpochSpecs::from(bank_forks.clone());
+
+        // initialize cluster slots with the current root bank
+        let root_bank = bank_forks.read().unwrap().root_bank();
+        cluster_slots.update(&root_bank, &cluster_info);
+
         while !exit.load(Ordering::Relaxed) {
             let slots = match cluster_slots_update_receiver.recv_timeout(Duration::from_millis(200))
             {
