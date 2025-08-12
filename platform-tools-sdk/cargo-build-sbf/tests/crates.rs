9fc501a12c206286be0abc89ccc79400d546730d
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-08-12 10:22:46 -0300

    Configure `cargo-builld-sbf` tests properly with `cargo nextest` (#7384)
    * Revert "Use a single test for `cargo-build-sbf` (#7379)"
    
    This reverts commit f26d0477e067cdeb2d0fd6878bd58c3e4e2590e9.
    
    * Configure files

--- platform-tools-sdk/cargo-build-sbf/tests/crates.rs
+++ platform-tools-sdk/cargo-build-sbf/tests/crates.rs
@@ -1,15 +1,56 @@
 use {
     assert_cmd::assert::Assert,
     predicates::prelude::*,
-    std::{
-        env, fs,
-        path::PathBuf,
-        str::FromStr,
-        sync::atomic::{AtomicBool, Ordering},
-    },
+    std::{env, fs, path::PathBuf, str::FromStr},
 };
 
-static SBF_TOOLS_INSTALL: AtomicBool = AtomicBool::new(true);
+#[macro_use]
+extern crate serial_test;
+
+fn should_install_tools() -> bool {
+    let tools_path = env::var("HOME").unwrap();
+    let toolchain_path = PathBuf::from(tools_path)
+        .join(".cache")
+        .join("solana")
+        .join("v1.50")
+        .join("platform-tools");
+
+    let rust_path = toolchain_path.join("rust");
+    let llvm_path = toolchain_path.join("llvm");
+    let binaries = rust_path.join("bin");
+
+    let rustc = binaries.join(if cfg!(windows) { "rustc.exe" } else { "rustc" });
+    let cargo = binaries.join(if cfg!(windows) { "cargo.exe" } else { "cargo" });
+
+    if !toolchain_path.try_exists().unwrap_or(false)
+        || !rust_path.try_exists().unwrap_or(false)
+        || !llvm_path.try_exists().unwrap_or(false)
+        || !binaries.try_exists().unwrap_or(false)
+        || !rustc.try_exists().unwrap_or(false)
+        || !cargo.try_exists().unwrap_or(false)
+    {
+        return true;
+    }
+
+    let Ok(folder_metadata) = fs::metadata(rust_path) else {
+        return true;
+    };
+    let Ok(creation_time) = folder_metadata.created() else {
+        return true;
+    };
+
+    let now = std::time::SystemTime::now();
+    let Ok(elapsed_time) = now.duration_since(creation_time) else {
+        return true;
+    };
+
+    if elapsed_time.as_secs() > 300 {
+        return true;
+    }
+
+    false
+}
+
 fn run_cargo_build(crate_name: &str, extra_args: &[&str], fail: bool) {
     let cwd = env::current_dir().expect("Unable to get current working directory");
     let toml = cwd
@@ -19,7 +60,7 @@ fn run_cargo_build(crate_name: &str, extra_args: &[&str], fail: bool) {
         .join("Cargo.toml");
     let toml = format!("{}", toml.display());
     let mut args = vec!["-v", "--sbf-sdk", "../sbf", "--manifest-path", &toml];
-    if SBF_TOOLS_INSTALL.fetch_and(false, Ordering::SeqCst) {
+    if should_install_tools() {
         args.push("--force-tools-install");
     }
     for arg in extra_args {
@@ -51,11 +92,15 @@ fn clean_target(crate_name: &str) {
     fs::remove_dir_all(target).expect("Failed to remove target dir");
 }
 
+#[test]
+#[serial]
 fn test_build() {
     run_cargo_build("noop", &[], false);
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_dump() {
     // This test requires rustfilt.
     assert_cmd::Command::new("cargo")
@@ -75,6 +120,8 @@ fn test_dump() {
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_out_dir() {
     run_cargo_build("noop", &["--sbf-out-dir", "tmp_out"], false);
     let cwd = env::current_dir().expect("Unable to get current working directory");
@@ -84,6 +131,8 @@ fn test_out_dir() {
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_target_dir() {
     let target_dir = "./temp-target-dir";
     run_cargo_build("noop", &["--lto", "--", "--target-dir", target_dir], false);
@@ -98,6 +147,8 @@ fn test_target_dir() {
     fs::remove_dir_all(target_dir).expect("Failed to remove custom target dir");
 }
 
+#[test]
+#[serial]
 fn test_target_and_out_dir() {
     let target_dir = "./temp-target-dir";
     run_cargo_build(
@@ -114,6 +165,8 @@ fn test_target_and_out_dir() {
     fs::remove_dir_all(target_dir).expect("Failed to remove custom target dir");
 }
 
+#[test]
+#[serial]
 fn test_generate_child_script_on_failure() {
     run_cargo_build("fail", &["--generate-child-script-on-failure"], true);
     let cwd = env::current_dir().expect("Unable to get current working directory");
@@ -155,6 +208,8 @@ fn build_noop_and_readelf(arch: &str) -> Assert {
     assert_cmd::Command::new(readelf).args(["-h", bin]).assert()
 }
 
+#[test]
+#[serial]
 fn test_sbpfv0() {
     let assert_v0 = build_noop_and_readelf("v0");
     assert_v0
@@ -165,6 +220,8 @@ fn test_sbpfv0() {
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_sbpfv1() {
     let assert_v1 = build_noop_and_readelf("v1");
     assert_v1
@@ -175,6 +232,8 @@ fn test_sbpfv1() {
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_sbpfv2() {
     let assert_v1 = build_noop_and_readelf("v2");
     assert_v1
@@ -185,6 +244,8 @@ fn test_sbpfv2() {
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_sbpfv3() {
     let assert_v1 = build_noop_and_readelf("v3");
     assert_v1
@@ -195,6 +256,8 @@ fn test_sbpfv3() {
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_sbpfv4() {
     let assert_v1 = build_noop_and_readelf("v4");
     assert_v1
@@ -205,16 +268,22 @@ fn test_sbpfv4() {
     clean_target("noop");
 }
 
+#[test]
+#[serial]
 fn test_package_metadata_tools_version() {
     run_cargo_build("package-metadata", &[], false);
     clean_target("package-metadata");
 }
 
+#[test]
+#[serial]
 fn test_workspace_metadata_tools_version() {
     run_cargo_build("workspace-metadata", &[], false);
     clean_target("workspace-metadata");
 }
 
+#[test]
+#[serial]
 fn test_corrupted_toolchain() {
     run_cargo_build("noop", &[], false);
 
@@ -274,38 +343,4 @@ fn test_corrupted_toolchain() {
 
     // Revert to the original name, so other tests can run correctly.
     fs::rename(wrong_rust_folder, right_rust_folder).expect("Failed to rename file");
-}
-
-#[test]
-fn cargo_build_sbf_all_tests() {
-    // The cargo-build-sbf tests must download and install the platform tools only once and run
-    // sequentially afterwards.
-    //
-    // The usage of an atomic boolean to prevent multiple tests downloading the tools and the
-    // #[serial] attribute only work when we execute `cargo test`.
-    //
-    // `cargo nextest` used in CI executes each test in a separate process, so the atomic boolean
-    // has no effect, nor does the #[serial] attribute. This causes all tests to download the tools,
-    // locks the toml files, and frequently corrupts the installation. Splitting the repository's
-    // tests in partitions further exacerbates the problem, since the cargo-build-sbf are not even
-    // run in the same machine.
-    //
-    // Creating an omnibus function to execute all tests at once is the simplest solution to that.
-    // It does not use process synchronization to handle the installation, and does not need
-    // any special configuration in nexttest to prevent these tests from being split in partitions.
-
-    test_build();
-    test_dump();
-    test_out_dir();
-    test_target_dir();
-    test_target_and_out_dir();
-    test_generate_child_script_on_failure();
-    test_sbpfv0();
-    test_sbpfv1();
-    test_sbpfv2();
-    test_sbpfv3();
-    test_sbpfv4();
-    test_package_metadata_tools_version();
-    test_workspace_metadata_tools_version();
-    test_corrupted_toolchain();
 }
