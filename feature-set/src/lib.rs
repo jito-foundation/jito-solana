409946cfa1012e3572932524d3180363ccd9e5d4
Ashwin Sekar <ashwin@anza.xyz>
2025-08-12 14:54:52 -0400

    alpenglow: remove feature flag from `FEATURE_NAMES` (#7454)
    * alpenglow: remove feature flag from `FEATURE_NAMES`
    
    * pr feedback: leave initial line as a comment

--- feature-set/src/lib.rs
+++ feature-set/src/lib.rs
@@ -1099,6 +1099,8 @@ pub mod formalize_loaded_transaction_data_size {
     solana_pubkey::declare_id!("DeS7sR48ZcFTUmt5FFEVDr1v1bh73aAbZiZq3SYr8Eh8");
 }
 
+// Until alpenglow is fully upstreamed, this is intentionally left out of
+// `FEATURE_NAMES`
 pub mod alpenglow {
     solana_pubkey::declare_id!("mustRekeyVm2QHYB3JPefBiU4BY3Z6JkW2k3Scw5GWP");
 }
@@ -1358,7 +1360,7 @@ pub static FEATURE_NAMES: LazyLock<AHashMap<Pubkey, &'static str>> = LazyLock::n
         (enshrine_slashing_program::id(), "SIMD-0204: Slashable event verification"),
         (enable_extend_program_checked::id(), "Enable ExtendProgramChecked instruction"),
         (formalize_loaded_transaction_data_size::id(), "SIMD-0186: Loaded transaction data size specification"),
-        (alpenglow::id(), "Enable Alpenglow"),
+        // Intentionally left out until upstreaming is complete (alpenglow::id(), "Enable Alpenglow"),
         (disable_zk_elgamal_proof_program::id(), "Disables zk-elgamal-proof program"),
         (reenable_zk_elgamal_proof_program::id(), "Re-enables zk-elgamal-proof program"),
         (raise_block_limits_to_100m::id(), "SIMD-0286: Raise block limit to 100M"),
