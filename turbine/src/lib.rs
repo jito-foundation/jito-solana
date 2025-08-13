5f8b706a124d5fa460baa8034d8d364965d7d829
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-08-13 21:29:47 +0300

    hide turbine crate from external use (#7392)
    hide turbine crate

--- turbine/src/lib.rs
+++ turbine/src/lib.rs
@@ -1,16 +1,31 @@
 #![allow(clippy::arithmetic_side_effects)]
 
+#[cfg(feature = "agave-unstable-api")]
 mod addr_cache;
+
+#[cfg(feature = "agave-unstable-api")]
 pub mod broadcast_stage;
+
+#[cfg(feature = "agave-unstable-api")]
 pub mod cluster_nodes;
+
+#[cfg(feature = "agave-unstable-api")]
 pub mod quic_endpoint;
+
+#[cfg(feature = "agave-unstable-api")]
 pub mod retransmit_stage;
+
+#[cfg(feature = "agave-unstable-api")]
 pub mod sigverify_shreds;
+
+#[cfg(feature = "agave-unstable-api")]
 pub mod xdp;
 
+#[cfg(feature = "agave-unstable-api")]
 #[macro_use]
 extern crate log;
 
+#[cfg(feature = "agave-unstable-api")]
 #[macro_use]
 extern crate solana_metrics;
 
