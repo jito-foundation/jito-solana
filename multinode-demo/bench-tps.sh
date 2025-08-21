0ee477919c47b65f9d2e3d31ba962d21bb3b7145
Andrew Fitzgerald <apfitzge@gmail.com>
2025-08-21 17:40:02 -0500

    Remove --entrypoint from default args of bench-tps script (#7658)

--- multinode-demo/bench-tps.sh
+++ multinode-demo/bench-tps.sh
@@ -21,7 +21,6 @@ usage() {
 
 args=("$@")
 default_arg --url "http://127.0.0.1:8899"
-default_arg --entrypoint "127.0.0.1:8001"
 default_arg --faucet "127.0.0.1:9900"
 default_arg --duration 90
 default_arg --tx-count 50000
