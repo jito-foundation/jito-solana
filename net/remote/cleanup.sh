5a1ad600bfb8d171e62c25be10293464b75f7fc2
Alex Pyattaev <alex.pyattaev@anza.xyz>
2025-08-28 17:09:21 +0300

    remove stale integration tests that are not used (#7166)

--- net/remote/cleanup.sh
+++ net/remote/cleanup.sh
@@ -14,11 +14,6 @@ for pid in solana/*.pid; do
     $sudo kill -- -"$pgid"
   fi
 done
-if [[ -f solana/netem.cfg ]]; then
-  solana/scripts/netem.sh delete < solana/netem.cfg
-  rm -f solana/netem.cfg
-fi
-solana/scripts/net-shaper.sh cleanup
 for pattern in validator.sh boostrap-leader.sh solana- remote- iftop validator client node; do
   echo "killing $pattern"
   pkill -f $pattern
