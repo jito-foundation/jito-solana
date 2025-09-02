bef3d3611ceb7a0e5999f4f51bceb9d51d4e629a
Joe C <joe.caulfield@anza.xyz>
2025-09-02 11:26:02 -0400

    program-runtime: drop metrics (#7807)
    program-runtime: remove metrics dependency

--- program-runtime/src/lib.rs
+++ program-runtime/src/lib.rs
@@ -2,10 +2,6 @@
 #![deny(clippy::arithmetic_side_effects)]
 #![deny(clippy::indexing_slicing)]
 
-#[cfg(feature = "metrics")]
-#[macro_use]
-extern crate solana_metrics;
-
 pub use solana_sbpf;
 pub mod execution_budget;
 pub mod invoke_context;
