e14477a7b7067e5ffa02af563ad5e7e9b009225f
puhtaytow <18026645+puhtaytow@users.noreply.github.com>
2025-09-05 09:50:17 +0200

    ledger: blockstore prepare for removing unchained (#7878)
    * use chained variant in test_find_missing_data_indexes_timeout
    
    * use chained variant in test_should_insert_data_shred
    
    * remove test_cases / use chained varian always in test_duplicate_slot

--- ledger/src/blockstore.rs
+++ ledger/src/blockstore.rs
@@ -5336,7 +5336,6 @@ pub mod tests {
             InnerInstruction, InnerInstructions, Reward, Rewards, TransactionTokenBalance,
         },
         std::{cmp::Ordering, time::Duration},
-        test_case::test_case,
     };
 
     // used for tests only
@@ -6878,7 +6877,7 @@ pub mod tests {
                         &keypair,
                         &[],
                         false,
-                        None,
+                        Some(Hash::default()), // merkle_root
                         (i * gap) as u32,
                         (i * gap) as u32,
                         &reed_solomon_cache,
@@ -7061,7 +7060,7 @@ pub mod tests {
                 &keypair,
                 &entries,
                 true,
-                None, //chained_merkle_root
+                Some(Hash::default()), // merkle_root
                 0,
                 0,
                 &rsc,
@@ -7089,9 +7088,9 @@ pub mod tests {
                 &keypair,
                 &[],
                 true,
-                None, //chained_merkle_root
-                6,    // next_shred_index,
-                6,    // next_code_index
+                Some(Hash::default()), // merkle_root
+                6,                     // next_shred_index,
+                6,                     // next_code_index
                 &rsc,
                 &mut ProcessShredsStats::default(),
             )
@@ -7152,9 +7151,9 @@ pub mod tests {
                 &Keypair::new(),
                 &entries,
                 true,
-                None,     //chained_merkle_root
-                last_idx, // next_shred_index,
-                last_idx, // next_code_index
+                Some(Hash::default()), // merkle_root
+                last_idx,              // next_shred_index,
+                last_idx,              // next_code_index
                 &rsc,
                 &mut ProcessShredsStats::default(),
             )
@@ -10244,21 +10243,20 @@ pub mod tests {
         assert_eq!(num_coding_in_index, num_coding);
     }
 
-    #[test_case(false)]
-    #[test_case(true)]
-    fn test_duplicate_slot(chained: bool) {
+    #[test]
+    fn test_duplicate_slot() {
         let slot = 0;
         let entries1 = make_slot_entries_with_transactions(1);
         let entries2 = make_slot_entries_with_transactions(1);
         let leader_keypair = Arc::new(Keypair::new());
         let reed_solomon_cache = ReedSolomonCache::default();
         let shredder = Shredder::new(slot, 0, 0, 0).unwrap();
-        let chained_merkle_root = chained.then(|| Hash::new_from_array(rand::thread_rng().gen()));
+        let merkle_root = Some(Hash::new_from_array(rand::thread_rng().gen()));
         let (shreds, _) = shredder.entries_to_merkle_shreds_for_tests(
             &leader_keypair,
             &entries1,
             true, // is_last_in_slot
-            chained_merkle_root,
+            merkle_root,
             0, // next_shred_index
             0, // next_code_index,
             &reed_solomon_cache,
@@ -10268,7 +10266,7 @@ pub mod tests {
             &leader_keypair,
             &entries2,
             true, // is_last_in_slot
-            chained_merkle_root,
+            merkle_root,
             0, // next_shred_index
             0, // next_code_index
             &reed_solomon_cache,
