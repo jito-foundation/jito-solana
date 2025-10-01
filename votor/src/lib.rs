bac227d03c5a34ad0dda792328fd51b009ef28f0
Wen <113942165+wen-coding@users.noreply.github.com>
2025-10-01 13:01:20 -0700

    Make agave-unstable-api default for votor/ so we don't need to specify it many times. (#8257)
    * Make agave-unstable-api default for votor/ so we don't need to specify it many times.
    
    * Use default = [ "agave-unstable-api" ] in Cargo.toml instead, this makes `cargo test` work.
    
    * Make linter happy.
    
    * Should still put everything under agave-unstable-api.

--- votor/src/lib.rs
+++ votor/src/lib.rs
@@ -1,45 +1,21 @@
+#![cfg(feature = "agave-unstable-api")]
 #![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
 
-#[cfg(feature = "agave-unstable-api")]
-pub mod commitment;
-
-#[cfg(feature = "agave-unstable-api")]
-pub mod common;
-
-#[cfg(feature = "agave-unstable-api")]
-mod consensus_metrics;
-
-#[cfg(feature = "agave-unstable-api")]
-pub mod consensus_pool;
-
-#[cfg(feature = "agave-unstable-api")]
-pub mod event;
-
-#[cfg(feature = "agave-unstable-api")]
-pub mod root_utils;
-
-#[cfg(feature = "agave-unstable-api")]
 #[macro_use]
 extern crate log;
-
-#[cfg(feature = "agave-unstable-api")]
 extern crate serde_derive;
 
-#[cfg(feature = "agave-unstable-api")]
+pub mod commitment;
+pub mod common;
+mod consensus_metrics;
+pub mod consensus_pool;
+pub mod event;
+pub mod root_utils;
 mod staked_validators_cache;
-
-#[cfg(feature = "agave-unstable-api")]
 mod timer_manager;
-
-#[cfg(feature = "agave-unstable-api")]
 pub mod vote_history;
-#[cfg(feature = "agave-unstable-api")]
 pub mod vote_history_storage;
-
-#[cfg(feature = "agave-unstable-api")]
 mod voting_service;
-
-#[cfg(feature = "agave-unstable-api")]
 mod voting_utils;
 
 #[cfg_attr(feature = "frozen-abi", macro_use)]
