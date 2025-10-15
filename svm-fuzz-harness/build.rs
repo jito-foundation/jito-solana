9db91872482e44f2dfbb43d3e3d3386e120da62a
cmoyes-jump <cmoyes@jumptrading.com>
2025-10-14 22:44:07 -0500

    svm: re-land Firedancer testing harness without git submodules (#8416)
    * Revert "Revert integration of Firedancer's fuzz harness for instructions (#8047) (#8402)"
    
    This reverts commit 58863b0d265bb1f05f12fe05831b36426ea5d4a1.
    
    * Use Cargo links
    
    * Remove empty protosol directory
    
    * Remove test vectors script
    
    * normalize for windows
    
    * Move path normalization back up to producer
    
    * Update build tag
    
    * re-run lock script

---
+++ svm-fuzz-harness/build.rs
@@ -0,0 +1,40 @@
+use std::{env, fs, path::PathBuf};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get absolute proto dir from producer
+    let proto_dir = PathBuf::from(
+        env::var("DEP_PROTOSOL_PROTO_DIR")
+            .expect("protosol did not expose PROTO_DIR, did protosol build.rs run first?"),
+    );
+
+    println!("cargo:rerun-if-env-changed=DEP_PROTOSOL_PROTO_DIR");
+    println!("cargo:rerun-if-changed={}", proto_dir.display());
+
+    // Collect absolute .proto paths
+    let mut proto_files = vec![];
+    for entry in fs::read_dir(&proto_dir)? {
+        let path = entry?.path();
+        if path.extension().and_then(|e| e.to_str()) == Some("proto") {
+            println!("cargo:rerun-if-changed={}", path.display());
+            proto_files.push(path);
+        }
+    }
+
+    // Ensure deterministic order for rebuilds
+    proto_files.sort();
+
+    // Compile protos into Rust
+    let out_dir = PathBuf::from(env::var("OUT_DIR")?);
+    let mut config = prost_build::Config::new();
+    config.out_dir(&out_dir);
+
+    config.compile_protos(
+        &proto_files
+            .iter()
+            .map(|p| p.display().to_string())
+            .collect::<Vec<_>>(),
+        &[proto_dir.to_str().unwrap()],
+    )?;
+
+    Ok(())
+}
---
+++ svm-fuzz-harness/src/fixture/error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+#[derive(Debug, Error, PartialEq)]
+pub enum FixtureError {
+    #[error("Invalid fixture input")]
+    InvalidFixtureInput,
+
+    #[error("Invalid public key bytes")]
+    InvalidPubkeyBytes(Vec<u8>),
+
+    #[error("An account is missing for instruction account index {0}")]
+    AccountMissingForInstrAccount(usize),
+}
