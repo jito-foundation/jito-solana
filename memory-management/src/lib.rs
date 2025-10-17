89f620706d54301a7527fac06ef97d71510bc3d7
Lucas Ste <38472950+LucasSte@users.noreply.github.com>
2025-10-17 11:19:54 -0300

    🪓 Delete agave-memory-management 🪓 (#8521)
    Delete agave-memory-management

--- memory-management/src/lib.rs
+++
@@ -1,9 +0,0 @@
-#![deny(clippy::arithmetic_side_effects)]
-pub mod aligned_memory;
-
-/// Returns true if `ptr` is aligned to `align`.
-pub fn is_memory_aligned(ptr: usize, align: usize) -> bool {
-    ptr.checked_rem(align)
-        .map(|remainder| remainder == 0)
-        .unwrap_or(false)
-}
