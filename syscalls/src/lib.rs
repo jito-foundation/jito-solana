5ba294a0671c14cdc68d887de9fd06ae0e798b59
samkim-crypto <skim13@cs.stanford.edu>
2025-10-29 17:57:39 +0900

    Bump `solana-bn254` crate to v3.1 (#8472)
    * bump `solana-bn254` crate to v3.1
    
    * update to `V1` for bn128 multiplication since SIMD-222 is already active
    
    * re-organize imports
    
    * bump to v3.1.2

--- syscalls/src/lib.rs
+++ syscalls/src/lib.rs
@@ -25,11 +25,6 @@ use {
     crate::mem_ops::is_nonoverlapping,
     solana_big_mod_exp::{big_mod_exp, BigModExpParams},
     solana_blake3_hasher as blake3,
-    solana_bn254::prelude::{
-        alt_bn128_addition, alt_bn128_multiplication, alt_bn128_pairing,
-        ALT_BN128_ADDITION_OUTPUT_LEN, ALT_BN128_MULTIPLICATION_OUTPUT_LEN,
-        ALT_BN128_PAIRING_ELEMENT_LEN, ALT_BN128_PAIRING_OUTPUT_LEN,
-    },
     solana_cpi::MAX_RETURN_DATA,
     solana_hash::Hash,
     solana_instruction::{error::InstructionError, AccountMeta, ProcessedSiblingInstruction},
@@ -1572,20 +1567,28 @@ declare_builtin_function!(
         _arg5: u64,
         memory_mapping: &mut MemoryMapping,
     ) -> Result<u64, Error> {
-        use solana_bn254::prelude::{ALT_BN128_ADD, ALT_BN128_MUL, ALT_BN128_PAIRING};
+        use solana_bn254::versioned::{
+            alt_bn128_versioned_g1_addition, alt_bn128_versioned_g1_multiplication,
+            alt_bn128_versioned_pairing, Endianness, VersionedG1Addition,
+            VersionedG1Multiplication, VersionedPairing, ALT_BN128_ADDITION_OUTPUT_SIZE,
+            ALT_BN128_G1_ADD_BE, ALT_BN128_G1_MUL_BE, ALT_BN128_MULTIPLICATION_OUTPUT_SIZE,
+            ALT_BN128_PAIRING_BE, ALT_BN128_PAIRING_ELEMENT_SIZE,
+            ALT_BN128_PAIRING_OUTPUT_SIZE,
+        };
+
         let execution_cost = invoke_context.get_execution_cost();
         let (cost, output): (u64, usize) = match group_op {
-            ALT_BN128_ADD => (
+            ALT_BN128_G1_ADD_BE => (
                 execution_cost.alt_bn128_addition_cost,
-                ALT_BN128_ADDITION_OUTPUT_LEN,
+                ALT_BN128_ADDITION_OUTPUT_SIZE,
             ),
-            ALT_BN128_MUL => (
+            ALT_BN128_G1_MUL_BE => (
                 execution_cost.alt_bn128_multiplication_cost,
-                ALT_BN128_MULTIPLICATION_OUTPUT_LEN,
+                ALT_BN128_MULTIPLICATION_OUTPUT_SIZE,
             ),
-            ALT_BN128_PAIRING => {
+            ALT_BN128_PAIRING_BE => {
                 let ele_len = input_size
-                    .checked_div(ALT_BN128_PAIRING_ELEMENT_LEN as u64)
+                    .checked_div(ALT_BN128_PAIRING_ELEMENT_SIZE as u64)
                     .expect("div by non-zero constant");
                 let cost = execution_cost
                     .alt_bn128_pairing_one_pair_cost_first
@@ -1596,8 +1599,8 @@ declare_builtin_function!(
                     )
                     .saturating_add(execution_cost.sha256_base_cost)
                     .saturating_add(input_size)
-                    .saturating_add(ALT_BN128_PAIRING_OUTPUT_LEN as u64);
-                (cost, ALT_BN128_PAIRING_OUTPUT_LEN)
+                    .saturating_add(ALT_BN128_PAIRING_OUTPUT_SIZE as u64);
+                (cost, ALT_BN128_PAIRING_OUTPUT_SIZE)
             }
             _ => {
                 return Err(SyscallError::InvalidAttribute.into());
@@ -1618,19 +1621,34 @@ declare_builtin_function!(
             invoke_context.get_check_aligned(),
         )?;
 
-        let calculation = match group_op {
-            ALT_BN128_ADD => alt_bn128_addition,
-            ALT_BN128_MUL => alt_bn128_multiplication,
-            ALT_BN128_PAIRING => alt_bn128_pairing,
+        let result_point = match group_op {
+            ALT_BN128_G1_ADD_BE => {
+                alt_bn128_versioned_g1_addition(VersionedG1Addition::V0, input, Endianness::BE)
+            }
+            ALT_BN128_G1_MUL_BE => {
+                alt_bn128_versioned_g1_multiplication(
+                    VersionedG1Multiplication::V1,
+                    input,
+                    Endianness::BE
+                )
+            }
+            ALT_BN128_PAIRING_BE => {
+                alt_bn128_versioned_pairing(VersionedPairing::V0, input, Endianness::BE)
+            }
             _ => {
                 return Err(SyscallError::InvalidAttribute.into());
             }
         };
 
-        let Ok(result_point) = calculation(input) else { return Ok(1) };
-
-        call_result.copy_from_slice(&result_point);
-        Ok(SUCCESS)
+        match result_point {
+            Ok(point) => {
+                call_result.copy_from_slice(&point);
+                Ok(SUCCESS)
+            }
+            Err(_) => {
+                Ok(1)
+            }
+        }
     }
 );
 
@@ -1802,27 +1820,31 @@ declare_builtin_function!(
         _arg5: u64,
         memory_mapping: &mut MemoryMapping,
     ) -> Result<u64, Error> {
-        use solana_bn254::compression::prelude::{
-            alt_bn128_g1_compress, alt_bn128_g1_decompress, alt_bn128_g2_compress,
-            alt_bn128_g2_decompress, ALT_BN128_G1_COMPRESS, ALT_BN128_G1_DECOMPRESS,
-            ALT_BN128_G2_COMPRESS, ALT_BN128_G2_DECOMPRESS, G1, G1_COMPRESSED, G2, G2_COMPRESSED,
+        use solana_bn254::{
+            prelude::{ALT_BN128_G1_POINT_SIZE, ALT_BN128_G2_POINT_SIZE},
+            compression::prelude::{
+                alt_bn128_g1_compress, alt_bn128_g1_decompress, alt_bn128_g2_compress,
+                alt_bn128_g2_decompress, ALT_BN128_G1_COMPRESS_BE, ALT_BN128_G1_DECOMPRESS_BE,
+                ALT_BN128_G2_COMPRESS_BE, ALT_BN128_G2_DECOMPRESS_BE, ALT_BN128_G1_COMPRESSED_POINT_SIZE,
+                ALT_BN128_G2_COMPRESSED_POINT_SIZE,
+            }
         };
         let execution_cost = invoke_context.get_execution_cost();
         let base_cost = execution_cost.syscall_base_cost;
         let (cost, output): (u64, usize) = match op {
-            ALT_BN128_G1_COMPRESS => (
+            ALT_BN128_G1_COMPRESS_BE => (
                 base_cost.saturating_add(execution_cost.alt_bn128_g1_compress),
-                G1_COMPRESSED,
+                ALT_BN128_G1_COMPRESSED_POINT_SIZE,
             ),
-            ALT_BN128_G1_DECOMPRESS => {
-                (base_cost.saturating_add(execution_cost.alt_bn128_g1_decompress), G1)
+            ALT_BN128_G1_DECOMPRESS_BE => {
+                (base_cost.saturating_add(execution_cost.alt_bn128_g1_decompress), ALT_BN128_G1_POINT_SIZE)
             }
-            ALT_BN128_G2_COMPRESS => (
+            ALT_BN128_G2_COMPRESS_BE => (
                 base_cost.saturating_add(execution_cost.alt_bn128_g2_compress),
-                G2_COMPRESSED,
+                ALT_BN128_G2_COMPRESSED_POINT_SIZE,
             ),
-            ALT_BN128_G2_DECOMPRESS => {
-                (base_cost.saturating_add(execution_cost.alt_bn128_g2_decompress), G2)
+            ALT_BN128_G2_DECOMPRESS_BE => {
+                (base_cost.saturating_add(execution_cost.alt_bn128_g2_decompress), ALT_BN128_G2_POINT_SIZE)
             }
             _ => {
                 return Err(SyscallError::InvalidAttribute.into());
@@ -1844,25 +1866,25 @@ declare_builtin_function!(
         )?;
 
         match op {
-            ALT_BN128_G1_COMPRESS => {
+            ALT_BN128_G1_COMPRESS_BE => {
                 let Ok(result_point) = alt_bn128_g1_compress(input) else {
                     return Ok(1);
                 };
                 call_result.copy_from_slice(&result_point);
             }
-            ALT_BN128_G1_DECOMPRESS => {
+            ALT_BN128_G1_DECOMPRESS_BE => {
                 let Ok(result_point) = alt_bn128_g1_decompress(input) else {
                     return Ok(1);
                 };
                 call_result.copy_from_slice(&result_point);
             }
-            ALT_BN128_G2_COMPRESS => {
+            ALT_BN128_G2_COMPRESS_BE => {
                 let Ok(result_point) = alt_bn128_g2_compress(input) else {
                     return Ok(1);
                 };
                 call_result.copy_from_slice(&result_point);
             }
-            ALT_BN128_G2_DECOMPRESS => {
+            ALT_BN128_G2_DECOMPRESS_BE => {
                 let Ok(result_point) = alt_bn128_g2_decompress(input) else {
                     return Ok(1);
                 };
