7c8c4091c95490168a4dedb2ebb301a7668542c8
Trent Nelson <490004+t-nelson@users.noreply.github.com>
2025-11-03 14:20:21 -0700

    remove public interfaces from `agave-unstable-api` (#8818)
    * remove `solana-client` crate and its dependencies from `agave-unstable-api`
    
    * remove `agave-geyser-plugin-interface` from `agave-unstable-api`
    
    * remove `solana-cli-config` from `agave-unstable-api`

--- cli-config/src/lib.rs
+++ cli-config/src/lib.rs
@@ -1,12 +1,3 @@
-#![cfg_attr(
-    not(feature = "agave-unstable-api"),
-    deprecated(
-        since = "3.1.0",
-        note = "This crate has been marked for formal inclusion in the Agave Unstable API. From \
-                v4.0.0 onward, the `agave-unstable-api` crate feature must be specified to \
-                acknowledge use of an interface that may break without warning."
-    )
-)]
 //! Loading and saving the Solana CLI configuration file.
 //!
 //! The configuration file used by the Solana CLI includes information about the
--- geyser-plugin-interface/src/lib.rs
+++ geyser-plugin-interface/src/lib.rs
@@ -1,10 +1,1 @@
-#![cfg_attr(
-    not(feature = "agave-unstable-api"),
-    deprecated(
-        since = "3.1.0",
-        note = "This crate has been marked for formal inclusion in the Agave Unstable API. From \
-                v4.0.0 onward, the `agave-unstable-api` crate feature must be specified to \
-                acknowledge use of an interface that may break without warning."
-    )
-)]
 pub mod geyser_plugin_interface;
