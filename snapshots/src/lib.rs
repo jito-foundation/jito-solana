0f46857c8c8b6ed16bd06e55f92ba0084a82c6ac
Brooks <brooks@anza.xyz>
2025-11-05 18:08:20 -0500

    Adds rust-2024 lints to snapshots crate (#8898)

--- snapshots/src/lib.rs
+++ snapshots/src/lib.rs
@@ -7,6 +7,15 @@
                 acknowledge use of an interface that may break without warning."
     )
 )]
+// Activate some of the Rust 2024 lints to make the future migration easier.
+#![warn(if_let_rescope)]
+#![warn(keyword_idents_2024)]
+#![warn(missing_unsafe_on_extern)]
+#![warn(rust_2024_guarded_string_incompatible_syntax)]
+#![warn(rust_2024_incompatible_pat)]
+#![warn(tail_expr_drop_order)]
+#![warn(unsafe_attr_outside_unsafe)]
+#![warn(unsafe_op_in_unsafe_fn)]
 
 mod archive;
 mod archive_format;
