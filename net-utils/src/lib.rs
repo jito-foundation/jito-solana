73af3c20aa212d6b8c321f757eee94e76216978e
Kamil Skalski <kamil.skalski@gmail.com>
2025-11-10 23:25:37 +0800

    chore: deny already satisfied edition 2024 warnings for workspace (#8989)
    * clippy: deny already satisfied edition 2024 warnings for workspace
    
    * Remove warnings elevated to workspace from individual crates
    
    * clippy(dev-bins): deny already satisfied edition 2024 warnings

--- net-utils/src/lib.rs
+++ net-utils/src/lib.rs
@@ -12,8 +12,6 @@
 // Activate some of the Rust 2024 lints to make the future migration easier.
 #![warn(if_let_rescope)]
 #![warn(keyword_idents_2024)]
-#![warn(missing_unsafe_on_extern)]
-#![warn(rust_2024_guarded_string_incompatible_syntax)]
 #![warn(rust_2024_incompatible_pat)]
 #![warn(tail_expr_drop_order)]
 #![warn(unsafe_attr_outside_unsafe)]
