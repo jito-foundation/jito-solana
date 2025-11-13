34943b03f30b1b119551b99385b406cafda89a09
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-11-13 14:29:53 -0500

    fix(poh): don't process service messages if we have a record (#9047)

--- poh/src/record_channels.rs
+++ poh/src/record_channels.rs
@@ -58,6 +58,7 @@ pub fn record_channels(track_transaction_indexes: bool) -> (RecordSender, Record
     )
 }
 
+#[derive(Debug)]
 pub enum RecordSenderError {
     /// The channel is full, the record was not sent.
     Full,
@@ -84,6 +85,11 @@ pub struct RecordSender {
 }
 
 impl RecordSender {
+    #[cfg(test)]
+    pub(crate) fn is_empty(&self) -> bool {
+        self.sender.is_empty()
+    }
+
     pub fn try_send(&self, record: Record) -> Result<Option<usize>, RecordSenderError> {
         let num_transactions: usize = record
             .transaction_batches
