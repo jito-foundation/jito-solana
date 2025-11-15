360411c0e576e3e96e285ab872924fe4432fc2b5
steviez <steven@anza.xyz>
2025-11-14 21:42:13 -0600

    blockstore: Remove slow test_purge_huge (#9087)
    We have similar correctness coverage with another test so we don't get
    much from this test inserting 1000's of slots

--- ledger/tests/blockstore.rs
+++ ledger/tests/blockstore.rs
@@ -2,7 +2,7 @@ use {
     solana_entry::entry,
     solana_hash::Hash,
     solana_ledger::{
-        blockstore::{self, make_many_slot_entries, test_all_empty_or_min, Blockstore},
+        blockstore::{self, Blockstore},
         get_tmp_ledger_path_auto_delete,
     },
     std::{sync::Arc, thread::Builder},
@@ -45,16 +45,4 @@ fn test_multiple_threads_insert_shred() {
         // Delete slots for next iteration
         blockstore.purge_and_compact_slots(0, num_threads + 1);
     }
-}
-
-#[test]
-fn test_purge_huge() {
-    let ledger_path = get_tmp_ledger_path_auto_delete!();
-    let blockstore = Blockstore::open(ledger_path.path()).unwrap();
-
-    let (shreds, _) = make_many_slot_entries(0, 5000, 10);
-    blockstore.insert_shreds(shreds, None, false).unwrap();
-
-    blockstore.purge_and_compact_slots(0, 4999);
-    test_all_empty_or_min(&blockstore, 5000);
 }
