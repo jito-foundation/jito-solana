5c38a65e7f5a566c278cb599c9d8c62a815dc7b7
David Gorski <gorski.dave@gmail.com>
2026-01-22 18:05:35 -0500

    Fix the fetch stage manager basics

--- core/src/tpu.rs
+++ core/src/tpu.rs
@@ -389,6 +389,7 @@ impl Tpu {
             exit.clone(),
         );
 
+        let bam_tpu_info = Arc::new(ArcSwap::new(Arc::new(None)));
         let (heartbeat_tx, heartbeat_rx) = unbounded();
         let fetch_stage_manager = FetchStageManager::new(
             cluster_info.clone(),
@@ -398,6 +399,7 @@ impl Tpu {
             exit.clone(),
             bam_enabled.clone(),
             cluster_info.my_contact_info().clone(),
+            bam_tpu_info.clone(),
         );
 
         let relayer_stage = RelayerStage::new(
@@ -438,6 +440,7 @@ impl Tpu {
             block_builder_fee_info: Arc::new(ArcSwap::from_pointee(BlockBuilderFeeInfo::default())),
             bam_node_pubkey: Arc::new(ArcSwap::from_pointee(Pubkey::default())),
             bank_forks: bank_forks.clone(),
+            bam_tpu_info,
         };
 
         let mut blacklisted_accounts = HashSet::new();
