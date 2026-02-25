f34231b817ac0b97b710474cc3bb99c515e11235
Lucas Bruder <lucas@jito.wtf>
2026-02-25 13:50:37 -0600

    ignore ring

--- ci/do-audit.sh
+++ ci/do-audit.sh
@@ -78,6 +78,15 @@ cargo_audit_ignores=(
 	# Severity:  6.8 (medium)
 	# Solution:  Upgrade to >=0.3.47
 	--ignore RUSTSEC-2026-0009
+
+  # Crate:     ring
+  # Version:   0.16.20
+  # Title:     Some AES functions may panic when overflow checking is enabled.
+  # Date:      2025-03-06
+  # ID:        RUSTSEC-2025-0009
+  # URL:       https://rustsec.org/advisories/RUSTSEC-2025-0009
+  # Solution:  Upgrade to >=0.17.12
+  --ignore RUSTSEC-2025-0009
 )
 scripts/cargo-for-all-lock-files.sh audit "${cargo_audit_ignores[@]}" | $dep_tree_filter
 # we want the `cargo audit` exit code, not `$dep_tree_filter`'s
