b20602ca4283e14d710957c43dc16a352a531062
Trent Nelson <490004+t-nelson@users.noreply.github.com>
2026-03-03 19:45:24 -0700

    Revert "fix: align xdp ebpf program bytes as required by the kernel ebpf parser (#10823)" (#10949)
    This reverts commit 424fac54cd8f835a6afbdc646909311c1251514f.

--- xdp-ebpf/src/lib.rs
+++ xdp-ebpf/src/lib.rs
@@ -8,22 +8,16 @@
 #![warn(unsafe_op_in_unsafe_fn)]
 #![no_std]
 
-#[repr(C, align(4))]
-pub struct Aligned<Bytes: ?Sized> {
-    pub _align: u8,
-    pub bytes: Bytes,
-}
-
-#[cfg(all(target_os = "linux", not(target_arch = "bpf")))]
-macro_rules! program {
-    () => {
-        include_bytes!(concat!(env!("CARGO_MANIFEST_DIR"), "/agave-xdp-prog"))
-    };
-}
-
 #[cfg(all(target_os = "linux", not(target_arch = "bpf")))]
 #[unsafe(no_mangle)]
-pub static AGAVE_XDP_EBPF_PROGRAM: &Aligned<[u8; program!().len()]> = &Aligned {
-    _align: 0,
-    bytes: *program!(),
+pub static AGAVE_XDP_EBPF_PROGRAM: [u8; aya::include_bytes_aligned!(concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/agave-xdp-prog"
+))
+.len()] = unsafe {
+    core::ptr::read(
+        aya::include_bytes_aligned!(concat!(env!("CARGO_MANIFEST_DIR"), "/agave-xdp-prog"))
+            .as_ptr()
+            .cast(),
+    )
 };
