bacacabe9e267a5c385e4b6b57c822dd181375e3
steviez <steven@anza.xyz>
2026-03-08 11:42:12 -0500

    chore: Remove frozen-abi from RuntimeConfig (#11099)
    RuntimeConfig was initially introduced as a field in Bank and thus
    needed to be stable since Bank's are serialized for snapshots. This
    struct is no longer a field in Bank so frozen-abi is no longer necessary
    either.

--- runtime/src/runtime_config.rs
+++ runtime/src/runtime_config.rs
@@ -1,13 +1,5 @@
 use solana_compute_budget::compute_budget::ComputeBudget;
 
-#[cfg(feature = "frozen-abi")]
-impl ::solana_frozen_abi::abi_example::AbiExample for RuntimeConfig {
-    fn example() -> Self {
-        // RuntimeConfig is not Serialize so just rely on Default.
-        RuntimeConfig::default()
-    }
-}
-
 /// Encapsulates flags that can be used to tweak the runtime behavior.
 #[derive(Debug, Default, Clone)]
 pub struct RuntimeConfig {
