68f1e9086c16cd6bff6c86508504605c899193cc
Stanislav Ladyzhenskiy <LStan@users.noreply.github.com>
2026-03-11 01:14:14 +0700

    compute-budget: Remove unused dependencies and features (#11156)
    Remove unused dependencies and features

--- compute-budget/src/lib.rs
+++ compute-budget/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg(feature = "agave-unstable-api")]
 //! Solana compute budget types and default configurations.
-#![cfg_attr(feature = "frozen-abi", feature(min_specialization))]
 
 pub mod compute_budget;
 pub mod compute_budget_limits;
