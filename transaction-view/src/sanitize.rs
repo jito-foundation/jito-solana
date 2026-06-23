81ded8ceb75beb7be2c91cf471c96bd920dcf855
Kamil Skalski <kamil.skalski@gmail.com>
2026-06-23 09:54:48 +0200

    clippy(tx-metadata): fix collapsible_if (#13364)
    * clippy(tx-metadata): fix collapsible_if
    
    * fmt

--- transaction-view/src/sanitize.rs
+++ transaction-view/src/sanitize.rs
@@ -181,10 +181,10 @@ fn sanitize_instructions(
             }
         }
 
-        if let Some(max_accounts_per_instruction) = config.max_accounts_per_instruction {
-            if instruction.accounts.len() > max_accounts_per_instruction {
-                return Err(TransactionViewError::SanitizeError);
-            }
+        if let Some(max_accounts_per_instruction) = config.max_accounts_per_instruction
+            && instruction.accounts.len() > max_accounts_per_instruction
+        {
+            return Err(TransactionViewError::SanitizeError);
         }
     }
 
