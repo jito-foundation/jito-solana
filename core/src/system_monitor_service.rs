ae4a27b5b90719fec026f8bb560740ba0b28c5e0
Trent Nelson <490004+t-nelson@users.noreply.github.com>
2026-07-01 21:37:57 -0600

    system-monitor-service: de-noise "os-config" metrics submission (#13615)
    * fix(system-monitor): submit os-config metrics as a single datapoint instead of seven
    
    * chore(system-monitor): demote os-config metrics datapoint to info

--- core/src/system_monitor_service.rs
+++ core/src/system_monitor_service.rs
@@ -13,6 +13,7 @@ use {
     },
 };
 use {
+    solana_metrics::datapoint::DataPoint,
     solana_time_utils::AtomicInterval,
     std::{
         collections::HashMap,
@@ -623,12 +624,13 @@ impl SystemMonitorService {
 
     #[cfg_attr(not(target_os = "linux"), allow(dead_code))]
     fn linux_report_network_limits(
+        mut datapoint: DataPoint,
         current_limits: &[(&'static str, &'static InterestingLimit, i64)],
     ) -> bool {
-        current_limits
+        let ret = current_limits
             .iter()
             .all(|(key, interesting_limit, current_value)| {
-                datapoint_warn!("os-config", (key, *current_value, i64));
+                datapoint.add_field_i64(key, *current_value);
                 match interesting_limit {
                     InterestingLimit::Recommend(recommended_value)
                         if current_value < recommended_value =>
@@ -648,7 +650,9 @@ impl SystemMonitorService {
                         true
                     }
                 }
-            })
+            });
+        solana_metrics::submit(datapoint, log::Level::Info);
+        ret
     }
 
     #[cfg(not(target_os = "linux"))]
@@ -659,9 +663,10 @@ impl SystemMonitorService {
 
     #[cfg(target_os = "linux")]
     pub fn check_os_network_limits() -> bool {
-        datapoint_info!("os-config", ("platform", platform_id(), String));
+        let mut datapoint = DataPoint::new("os-config");
+        datapoint.add_field_str("platform", platform_id().as_str());
         let current_limits = Self::linux_get_current_network_limits();
-        Self::linux_report_network_limits(&current_limits)
+        Self::linux_report_network_limits(datapoint, &current_limits)
     }
 
     #[cfg(target_os = "linux")]
