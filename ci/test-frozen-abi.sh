24a9fa77da5d04e381713599b5ca18c9beee5198
Akhilesh Singhania <akhi3030@gmail.com>
2026-07-29 10:39:01 +0200

    Runs frozen abi tests with agave-unstable-api (#14173)
    * Runs frozen abi tests with agave-unstable-api
    
    * apply feature unconditionally

--- ci/test-frozen-abi.sh
+++ ci/test-frozen-abi.sh
@@ -11,7 +11,7 @@ source "$here/rust-version.sh" nightly
 
 packages=$(cargo +"$rust_nightly" metadata --no-deps --format-version=1 | jq -r '.packages[] | select(.features | has("frozen-abi")) | .name')
 for package in $packages; do
-  cmd="cargo +$rust_nightly test -p $package --features frozen-abi --lib -- test_abi_digest test_api_digest --nocapture"
+  cmd="cargo +$rust_nightly test -p $package --features frozen-abi,agave-unstable-api --lib -- test_abi_digest test_api_digest --nocapture"
   echo "--- $cmd"
   $cmd
 done
