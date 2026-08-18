1c842992c0f0d55734200056e2a3f066df4138fd
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2026-08-18 07:13:14 -0700

    Share Jito validator CLI arguments

--- validator/src/commands/block_engine/mod.rs
+++ validator/src/commands/block_engine/mod.rs
@@ -1,42 +1,19 @@
 use {
-    crate::{admin_rpc_service, cli::DefaultArgs, commands::Result},
-    clap::{App, Arg, ArgMatches, SubCommand, value_t_or_exit},
+    crate::{
+        admin_rpc_service,
+        cli::DefaultArgs,
+        commands::{Result, jito_args},
+    },
+    clap::{App, ArgMatches, SubCommand, value_t_or_exit},
     std::path::Path,
 };
 
 pub fn command(_default_args: &DefaultArgs) -> App<'_, '_> {
     SubCommand::with_name("set-block-engine-config")
         .about("Set configuration for connection to a block engine")
-        .arg(
-            Arg::with_name("block_engine_url")
-                .long("block-engine-url")
-                .help(
-                    "URL entrypoint to the Block Engine. Connected Block Engine will be \
-                     autoconfigured unless `--disable-block-engine-autoconfig` is used. Set to \
-                     empty string to disable block engine connection.",
-                )
-                .takes_value(true)
-                .required(true),
-        )
-        .arg(
-            Arg::with_name("disable_block_engine_autoconfig")
-                .long("disable-block-engine-autoconfig")
-                .takes_value(false)
-                .help(
-                    "Disables Block Engine auto-configuration. This stops the validator client \
-                     from using the most performant Block Engine region. Values provided to \
-                     `--block-engine-url` will be used as-is.",
-                ),
-        )
-        .arg(
-            Arg::with_name("trust_block_engine_packets")
-                .long("trust-block-engine-packets")
-                .takes_value(false)
-                .help(
-                    "Skip signature verification on block engine packets. Not recommended unless \
-                     the block engine is trusted.",
-                ),
-        )
+        .arg(jito_args::block_engine_url().required(true))
+        .arg(jito_args::disable_block_engine_autoconfig())
+        .arg(jito_args::trust_block_engine_packets())
 }
 
 pub fn execute(subcommand_matches: &ArgMatches, ledger_path: &Path) -> Result<()> {
