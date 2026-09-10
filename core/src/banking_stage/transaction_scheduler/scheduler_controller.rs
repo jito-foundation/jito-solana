27ba21c08fbd392da6e84b15f833a60f7b946272
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2026-09-09 18:46:06 -0700

    banking_stage: preserve BAM cost admission order (JSA-72) (#1618)

--- core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
+++ core/src/banking_stage/transaction_scheduler/scheduler_controller.rs
@@ -1109,6 +1109,7 @@ mod tests {
                     revert_on_error: false,
                     respond_with_extra_info: false,
                     max_schedule_slot: None,
+                    admission: None,
                 },
                 retryable_indexes: vec![],
                 extra_info: None,
@@ -1505,6 +1506,7 @@ mod tests {
             finished_work_receiver,
             response_sender,
             shared_leader_state.clone(),
+            None,
         );
 
         let mut controller = SchedulerController::new(
