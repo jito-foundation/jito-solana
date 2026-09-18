655a5f318346247464d83cb4dbd4b317832be809
sam0x17 <sam@durosoft.com>
2026-09-18 23:27:34 +0000

    geyser: use external plugin interface crate (#15388)
    geyser: use external plugin interface

--- geyser-plugin-interface/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod block_footer;
-pub mod geyser_plugin_interface;
-pub mod transaction_status_meta;
--- scripts/patch-crates.sh
+++ scripts/patch-crates.sh
@@ -26,7 +26,6 @@ update_solana_dependencies() {
     solana-entry
     solana-faucet
     solana-fee
-    agave-geyser-plugin-interface
     solana-geyser-plugin-manager
     solana-gossip
     solana-lattice-hash
@@ -110,7 +109,6 @@ patch_crates_io_solana_no_header() {
   crates_map+=("solana-entry entry")
   crates_map+=("solana-faucet faucet")
   crates_map+=("solana-fee fee")
-  crates_map+=("agave-geyser-plugin-interface geyser-plugin-interface")
   crates_map+=("solana-geyser-plugin-manager geyser-plugin-manager")
   crates_map+=("solana-gossip gossip")
   crates_map+=("solana-lattice-hash lattice-hash")
